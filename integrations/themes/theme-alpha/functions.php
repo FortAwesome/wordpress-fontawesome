17494ff5faacc979edcc0427134cfcf3d096d81b
Mike Wilkerson <mwilkerson@gmail.com>
2018-06-09 06:51:06 -0700

    Add a theme to integrate with plugin
    - Simple child theme of one of the built-in themes
    - It will be a test client of our plugin for loading its icons

---
+++ integrations/themes/theme-alpha/functions.php
@@ -0,0 +1,13 @@
+<?php
+add_action( 'wp_enqueue_scripts', function (){
+  $parent_style = 'twentyseventeen';
+  // parent
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+  // child
+  wp_enqueue_style( 'theme-alpha',
+      get_stylesheet_directory_uri() . '/style.css',
+      array( $parent_style ),
+      wp_get_theme()->get('Version')
+  );
+})
+?>
