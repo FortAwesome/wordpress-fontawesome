30ff7c571c9b7dd681db3baf7aa35bd220e5be70
Mike Wilkerson <mwilkerson@gmail.com>
2018-06-12 11:57:38 -0700

    Refine test theme
    - remove most extraneous content from front page to ease
      visual inspection during development.
    - add an icon to allow for visual verification that icons
      are loading correctly.
    - use the plugin to register requirements (trivial, for now)

--- integrations/themes/theme-alpha/functions.php
+++ integrations/themes/theme-alpha/functions.php
@@ -1,4 +1,6 @@
 <?php
+define('THEME_ALPHA_LOG_PREFIX', 'theme-alpha');
+
 add_action( 'wp_enqueue_scripts', function (){
   $parent_style = 'twentyseventeen';
   // parent
@@ -9,5 +11,22 @@ add_action( 'wp_enqueue_scripts', function (){
       array( $parent_style ),
       wp_get_theme()->get('Version')
   );
-})
+});
+
+add_action('after_switch_theme', function(){
+  error_log(THEME_ALPHA_LOG_PREFIX . ': switching theme to theme-alpha');
+});
+
+add_action('font_awesome_dependencies', function(){
+  if ( class_exists('FontAwesomePlumbing') ) {
+    FontAwesomePlumbing()->register_dependency(array("client" => THEME_ALPHA_LOG_PREFIX));
+  }
+});
+
+add_action('font_awesome_enqueued', function($method, $host, $ver){
+  if ( class_exists('FontAwesomePlumbing') ) {
+    error_log( THEME_ALPHA_LOG_PREFIX . " font_awesome_enqueued: " . "method: " . $method . ", host: " . $host . ", ver: " . $ver);
+  }
+}, 10, 3);
 ?>
+
