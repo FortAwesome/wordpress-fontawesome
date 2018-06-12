e0ee04631171e875cfef12ef5c6f0b4737021bda
Mike Wilkerson <mwilkerson@gmail.com>
2018-06-12 12:00:05 -0700

    Add client plugin for development and testing
    - register requirements (trivially, for now)
    - filter the_content on posts to display some
      content involving icons to make it easy to
      visually inspect during development.

---
+++ integrations/plugins/plugin-beta/plugin-beta.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Plugin Name:       Plugin Beta
+ * Plugin URI:        https://fontawesome.com/plugin-beta/
+ * Description:       Test Client Plugin that depends upon Font Awesome Plumbing
+ * Version:           0.0.1
+ * Author:            Font Awesome
+ * Author URI:        https://fontawesome.com/
+ * License:           UNLICENSED
+ */
+
+defined( 'WPINC' ) || die;
+define( 'BETA_PLUGIN_VERSION', '0.0.1' );
+define( 'BETA_PLUGIN_LOG_PREFIX', 'beta-plugin' );
+
+add_action('font_awesome_dependencies', function(){
+  if ( class_exists('FontAwesomePlumbing') ) {
+    FontAwesomePlumbing()->register_dependency(array("client" => BETA_PLUGIN_LOG_PREFIX));
+  }
+});
+
+add_action('font_awesome_enqueued', function($method, $host, $ver){
+  if ( class_exists('FontAwesomePlumbing') ) {
+    error_log( BETA_PLUGIN_LOG_PREFIX . " font_awesome_enqueued: " . "method: " . $method . ", host: " . $host . ", ver: " . $ver);
+  }
+}, 10, 3);
+
+add_filter('the_content', function($content){
+  $pre_content = <<<EOT
+<div class="plugin-beta-pre-content">
+  <p>Expected by plugin-beta: "fab fa-font-awesome": <i class="fab fa-font-awesome"></i></p>
+</div>
+EOT;
+  return $pre_content . $content;
+}, 10, 1);
