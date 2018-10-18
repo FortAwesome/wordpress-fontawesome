5c9c0c8cfc9621c16b10d6d92c580a17adc7783e
Mike Wilkerson <mwilkerson@gmail.com>
2018-10-18 14:32:05 -0700

    Use the real fontawesome.com releases API

--- font-awesome/defines.php
+++ font-awesome/defines.php
@@ -2,5 +2,5 @@
 
 defined('FONTAWESOME_DIR_PATH') || define( 'FONTAWESOME_DIR_PATH', plugin_dir_path( __FILE__ ) );
 defined('FONTAWESOME_DIR_URL') || define( 'FONTAWESOME_DIR_URL', plugin_dir_url( __FILE__ ) );
-defined('FONTAWESOME_API_URL') || define( 'FONTAWESOME_API_URL', 'https://5bfdfec8.ngrok.io' );
+defined('FONTAWESOME_API_URL') || define( 'FONTAWESOME_API_URL', 'https://fontawesome.com' );
 
