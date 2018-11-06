867c64473730a2d30b6d74ccaca304bf8977199d
Mike Wilkerson <mwilkerson@gmail.com>
2018-11-06 11:25:00 -0800

    Expand config resource to include all relevant data

--- font-awesome-official/includes/class-font-awesome-config-controller.php
+++ font-awesome-official/includes/class-font-awesome-config-controller.php
@@ -33,7 +33,21 @@ class FontAwesomeConfigController extends WP_REST_Controller {
    * @return WP_Error|WP_REST_Response
    */
   public function get_item( $request ) {
-    $data = FontAwesome()->options();
+    $fa = FontAwesome();
+    $data = array(
+      "options" => $fa->options(),
+      "clientRequirements" => $fa->requirements(),
+      "conflicts" => $fa->conflicts(),
+      "currentLoadSpec" => $fa->load_spec(),
+      "unregisteredClients" => $fa->unregistered_clients(),
+      "releases" => array(
+        "available" => $fa->get_available_versions(),
+        "latest_version" => $fa->get_latest_version(),
+        "latest_semver" => $fa->get_latest_semver(),
+        "previous_version" => $fa->get_previous_version(),
+        "previous_semver" => $fa->get_previous_semver()
+      )
+    );
 
     return new WP_REST_Response( $data, 200 );
   }
