fca8c30f9974ebdb853d6284448027533867a398
Mike Wilkerson <mwilkerson@gmail.com>
2018-11-07 14:30:02 -0800

    Add more explanatory text and styling

--- font-awesome-official/admin/src/FontAwesomeAdminView.js
+++ font-awesome-official/admin/src/FontAwesomeAdminView.js
@@ -16,16 +16,26 @@ class FontAwesomeAdminView extends React.Component {
 
     return <div className={ styles['font-awesome-admin-view'] }>
       <h1>Font Awesome</h1>
-      <div className={ classnames( styles['status'], styles[statusLabel] ) }>
-        <p>
+      <div>
+        <p className={ classnames( styles['status'], styles[statusLabel] ) }>
           <span className={ styles['status-label'] }>Status: </span><span className={ styles['status-value'] }>{ statusLabel }</span>
         </p>
         { hasConflict
           ?
-          <div></div>
+          <div>(not yet implemented)</div>
           :
           <div className="load-spec-container">
+            <h2>Current Load Specification</h2>
             <LoadSpecView spec={ data.currentLoadSpec } />
+            <h2>Options</h2>
+            <p className={ styles['options-disclaimer'] }>
+              You can tune these options according to your preferences, as long as your preferences
+              don't conflict with the specifications required by other plugins and themes that you've installed.
+            </p>
+            <p className={ styles['options-disclaimer'] }>
+              If conflicts are detected, they'll be shown below, and
+              you might be able to resolve them just by choosing different options here.
+            </p>
             <OptionsSetter releases={ data.releases } currentOptions={ data.options } putData={ putData }/>
           </div>
         }
