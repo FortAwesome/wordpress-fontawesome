0604d42b5164a4a31b501c71c5f2cd4bc196041b
Mike Wilkerson <mwilkerson@gmail.com>
2018-11-07 13:53:55 -0800

    Add CSS module and some styling

--- font-awesome-official/admin/src/FontAwesomeAdminView.js
+++ font-awesome-official/admin/src/FontAwesomeAdminView.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import styles from './FontAwesomeAdminView.module.css'
 import LoadSpecView from './LoadSpecView'
 import OptionsSetter from './OptionsSetter'
 
@@ -13,20 +14,17 @@ class FontAwesomeAdminView extends React.Component {
 
     const statusLabel = hasConflict ? 'conflict' : 'good'
 
-    const statusClasses = classnames( 'status', statusLabel )
-
-    return <div className="font-awesome-admin-view">
+    return <div className={ styles['font-awesome-admin-view'] }>
       <h1>Font Awesome</h1>
-      <div className={statusClasses}>
+      <div className={ classnames( styles['status'], styles[statusLabel] ) }>
         <p>
-          <span className="status-label">Status: </span><span className="status-value">{ statusLabel }</span>
+          <span className={ styles['status-label'] }>Status: </span><span className={ styles['status-value'] }>{ statusLabel }</span>
         </p>
         { hasConflict
           ?
           <div></div>
           :
           <div className="load-spec-container">
-            <p><span className="loaded-label">Loaded: </span></p>
             <LoadSpecView spec={ data.currentLoadSpec } />
             <OptionsSetter releases={ data.releases } currentOptions={ data.options } putData={ putData }/>
           </div>
