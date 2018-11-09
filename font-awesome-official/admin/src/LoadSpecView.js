72ec02a3661b5adb84e73afeaa5a8c3f14077a6e
Mike Wilkerson <mwilkerson@gmail.com>
2018-11-09 12:52:36 -0800

    Improve layout and rename internal component to help find it with react dev tools

--- font-awesome-official/admin/src/LoadSpecView.js
+++ font-awesome-official/admin/src/LoadSpecView.js
@@ -5,7 +5,7 @@ import styles from './LoadSpecView.module.css'
 
 const PRODUCT_BASE_NAME = 'Font Awesome'
 
-const Component = props => {
+const LoadSpecView = props => {
   const { spec: {
     method,
     v4shim: usingV4Shims,
@@ -16,23 +16,39 @@ const Component = props => {
 
   const licenseType = pro ? 'Pro' : 'Free'
 
-  return <div className={ styles['load-spec'] }>
+  return <div className={styles['load-spec']}>
     <h2>Current Load Specification</h2>
-    <table className={ classnames('widefat', 'striped') }>
+    <table className={classnames('widefat', 'striped')}>
       <tbody>
-        <tr><td className={ styles['label'] }>Product</td><td className={ styles['value'] }>{ PRODUCT_BASE_NAME } <span className={ classnames('license', licenseType) }>{ licenseType }</span></td></tr>
-        <tr><td className={ styles['label'] }>Version</td><td className={ styles['value'] }>{ version }</td></tr>
-        <tr><td className={ styles['label'] }>Method</td><td className={ styles['value'] }>{ method }</td></tr>
-        <tr><td className={ styles['label'] }>Version 4 compatibility</td><td className={ styles['value'] }>{ usingV4Shims ? 'true' : 'false' }</td></tr>
-        <tr><td className={ styles['label'] }>Pseudo-elements support</td><td className={ styles['value'] }>{ usingPseudoElements ? 'true' : 'false' }</td></tr>
+      <tr>
+        <td className={styles['label']}>Product</td>
+        <td className={styles['value']}>{PRODUCT_BASE_NAME} <span
+          className={classnames('license', licenseType)}>{ licenseType }</span></td>
+      </tr>
+      <tr>
+        <td className={styles['label']}>Version</td>
+        <td className={styles['value']}>{version}</td>
+      </tr>
+      <tr>
+        <td className={styles['label']}>Method</td>
+        <td className={styles['value']}>{method}</td>
+      </tr>
+      <tr>
+        <td className={styles['label']}>Version 4 compatibility</td>
+        <td className={styles['value']}>{usingV4Shims ? 'true' : 'false'}</td>
+      </tr>
+      <tr>
+        <td className={styles['label']}>Pseudo-elements support</td>
+        <td className={styles['value']}>{usingPseudoElements ? 'true' : 'false'}</td>
+      </tr>
       </tbody>
     </table>
   </div>
 }
 
-export default Component
+export default LoadSpecView
 
-Component.propTypes = {
+LoadSpecView.propTypes = {
   spec: PropTypes.object
 }
 
