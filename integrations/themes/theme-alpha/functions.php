5151cc52786ad71586f84b78c33a2e404f0c33c3
Mike Wilkerson <mwilkerson@gmail.com>
2018-11-16 13:09:15 -0800

    Refactor classes and styles for theme alpha pseudo elements
    we'll use this pattern in plugin-eta

--- integrations/themes/theme-alpha/functions.php
+++ integrations/themes/theme-alpha/functions.php
@@ -20,8 +20,7 @@ add_action('after_switch_theme', function(){
 add_action('font_awesome_requirements', function(){
   if ( class_exists('FontAwesome') ) {
     fa()->register(array(
-      "name" => THEME_ALPHA_LOG_PREFIX,
-//      "method" => 'webfont'
+      "name" => THEME_ALPHA_LOG_PREFIX
     ));
   }
 });
@@ -35,19 +34,19 @@ add_action('font_awesome_enqueued', function($loadSpec){
 function theme_alpha_fa_classes(){
   $fa = fa();
   $load_spec = $fa->load_spec();
-  $theme_alpha_class_list = [];
+  $class_list = [ 'theme-alpha' ];
 
   $fa->using_pro()
-    ? array_push($theme_alpha_class_list, 'theme-alpha-fa-license-pro')
-    : array_push($theme_alpha_class_list, 'theme-alpha-fa-license-free');
+    ? array_push($class_list, 'fa-license-pro')
+    : array_push($class_list, 'fa-license-free');
 
   strpos($load_spec['version'], '5.0.')
-    ? array_push($theme_alpha_class_list, 'theme-alpha-fa-version-5-0')
-    : array_push($theme_alpha_class_list, 'theme-alpha-fa-version-5-1');
+    ? array_push($class_list, 'fa-version-5-0')
+    : array_push($class_list, 'fa-version-5-1');
 
   ($load_spec['method'] == 'svg')
-    ? array_push($theme_alpha_class_list, 'theme-alpha-fa-method-svg')
-    : array_push($theme_alpha_class_list, 'theme-alpha-fa-method-webfont');
+    ? array_push($class_list, 'fa-method-svg')
+    : array_push($class_list, 'fa-method-webfont');
 
-  return implode(' ', $theme_alpha_class_list);
+  return implode(' ', $class_list);
 }
