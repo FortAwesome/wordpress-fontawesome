3ee6a7092fce117d6d5ebf731e5caa3a74f02c6f
Mike Wilkerson <mwilkerson@gmail.com>
2018-11-20 12:35:09 -0800

    Only load once

--- font-awesome-official.php
+++ font-awesome-official.php
@@ -13,24 +13,28 @@
  */
 
 defined( 'WPINC' ) || die;
-require_once __DIR__ . '/defines.php';
 
-register_activation_hook(
-	__FILE__,
-	function() {
-		require_once FONTAWESOME_DIR_PATH . 'includes/class-fontawesome-activator.php';
-		FontAwesome_Activator::activate();
-	}
-);
+if(! defined('FONT_AWESOME_OFFICIAL_LOADED')) {
+	require_once __DIR__ . '/defines.php';
 
-register_deactivation_hook(
-	__FILE__,
-	function() {
-		require_once FONTAWESOME_DIR_PATH . 'includes/class-fontawesome-deactivator.php';
-		FontAwesome_Deactivator::deactivate();
-	}
-);
+	register_activation_hook(
+		__FILE__,
+		function() {
+			require_once FONTAWESOME_DIR_PATH . 'includes/class-fontawesome-activator.php';
+			FontAwesome_Activator::activate();
+		}
+	);
 
-require_once FONTAWESOME_DIR_PATH . 'includes/class-fontawesome.php';
+	register_deactivation_hook(
+		__FILE__,
+		function() {
+			require_once FONTAWESOME_DIR_PATH . 'includes/class-fontawesome-deactivator.php';
+			FontAwesome_Deactivator::deactivate();
+		}
+	);
 
-fa()->run();
+	require_once FONTAWESOME_DIR_PATH . 'includes/class-fontawesome.php';
+
+	define('FONT_AWESOME_OFFICIAL_LOADED', 1);
+	fa()->run();
+}
