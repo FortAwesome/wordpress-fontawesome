9e9d13e7b0230b0a50764b359eecef5f3e20f7f5
Rob Madole <robmadole@gmail.com>
2018-12-07 23:36:35 -0600

    Link to Pro (#26)
    * Link to Pro
    
    * Add a link to manage domains

--- admin/src/OptionsSetter.js
+++ admin/src/OptionsSetter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner, faCheck, faSkull } from '@fortawesome/free-solid-svg-icons'
+import { faSpinner, faCheck, faSkull, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import styles from './OptionsSetter.module.css'
 import sharedStyles from './App.module.css'
 import classnames from 'classnames'
@@ -150,6 +150,11 @@ class OptionsSetter extends React.Component {
             </th>
             <td>
               <input name="use-pro" checked={ this.state.usePro } value={ this.state.usePro } type="checkbox" onChange={ this.handleProCheck }/>
+              <span className={styles["label-hint"]}>
+                Requires a subscription.
+                <a rel="noopener noreferrer" target="_blank" href="https://fontawesome.com/pro"><FontAwesomeIcon icon={faExternalLinkAlt} /> Learn more</a>
+                <a rel="noopener noreferrer" target="_blank" href="https://fontawesome.com/account/domains"><FontAwesomeIcon icon={faExternalLinkAlt} /> Manage my allowed domains</a>
+              </span>
             </td>
           </tr>
           <tr>
