51396182dcfb45faf8e1bb45c117f2cdaaa73560
Mike Wilkerson <mwilkerson@gmail.com>
2019-03-19 09:32:29 -0700

    Remove Spyc from dev dependencies so it's not required for running phpunit in CI

--- tests/_support/class-mock-fontawesome-releases.php
+++ tests/_support/class-mock-fontawesome-releases.php
@@ -2,6 +2,8 @@
 
 namespace FortAwesome;
 
+require_once FONTAWESOME_DIR_PATH . 'tests/fixtures/releases.php';
+
 use \PHPUnit\Framework\TestCase;
 
 /**
@@ -12,25 +14,9 @@ use \PHPUnit\Framework\TestCase;
 class Mock_FontAwesome_Releases extends TestCase {
 	public static $releases = null;
 
-	public static function load_releases() {
-		$mocked_releases = array();
-		$files           = glob( trailingslashit( FONTAWESOME_DIR_PATH ) . 'tests/fixtures/releases/release*.yml' );
-		foreach ( $files as $file ) {
-			$basename = basename( $file );
-			$matches  = [];
-			if ( preg_match( '/release-([0-9]+\.[0-9]+\.[0-9]+)\.yml/', $basename, $matches ) ) {
-				$release_data                   = \Spyc::YAMLLoad( $file );
-				$mocked_releases[ $matches[1] ] = $release_data;
-			}
-		}
-
-		self::$releases = $mocked_releases;
-
-	}
-
 	public static function releases() {
 		if ( null === self::$releases ) {
-			self::load_releases();
+			self::$releases = get_mocked_releases();
 		}
 		return self::$releases;
 	}
