ca45e1a6d863167334238ff4d4b8b80b2d88d4a0
Mike Wilkerson <mwilkerson@gmail.com>
2019-06-21 17:51:12 -0700

    Enqueue also on admin and login pages (#45)

--- integrations/plugins/plugin-delta/plugin-delta.php
+++ integrations/plugins/plugin-delta/plugin-delta.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Plugin Delta
  * Plugin URI:        https://fontawesome.com/
- * Description:       Unregistered Client: tries to enqueue its own v5.0.11 SVG/JS from use.fontawesome.com. For display, prepends a block before every blog post that displays an icon with classes "fas fa-cloud-download". This icon appeared in v5.0.11 and is only available in Pro.
+ * Description:       Unregistered Client: tries to enqueue its own v5.0.11 SVG/JS from use.fontawesome.com. For display, prepends a block before every blog post that displays an icon with classes "fas fa-cloud-download". This icon appeared in v5.0.11 and is only available in Pro. It adds the same output on the login page, and in the footer of admin pages.
  * Version:           0.0.1
  * Author:            Font Awesome
  * Author URI:        https://fontawesome.com/
@@ -14,15 +14,15 @@ defined( 'WPINC' ) || die;
 define( 'DELTA_PLUGIN_VERSION', '0.0.1' );
 define( 'DELTA_PLUGIN_LOG_PREFIX', 'delta-plugin' );
 
-add_action('init', function(){
-  wp_enqueue_script(
-    'DELTA_PLUGIN_LOG_PREFIX',
-    'https://use.fontawesome.com/releases/v5.0.11/js/all.js',
-    array(),
-    null,
-    false
-  );
-});
+foreach( ['wp_enqueue_scripts', 'admin_enqueue_scripts', 'login_enqueue_scripts'] as $action ) {
+	wp_enqueue_script(
+		'DELTA_PLUGIN_LOG_PREFIX',
+		'https://use.fontawesome.com/releases/v5.0.11/js/all.js',
+		array(),
+		null,
+		false
+	);
+}
 
 add_action('font_awesome_enqueued', function(){
 	error_log(
@@ -35,12 +35,24 @@ add_action('font_awesome_enqueued', function(){
 	);
 }, 10, 3);
 
-add_filter('the_content', function($content){
-  $pre_content = <<<EOT
+$pre_content = <<<EOT
 <div class="plugin-delta-pre-content" style="border: 1px solid grey;">
   <h2>Plugin Delta</h2>
   <p>Expected by plugin-delta (introduced v5.0.11): "fas fa-cloud-download": <i class="fas fa-cloud-download"></i></p>
 </div>
 EOT;
+
+add_filter('the_content', function($content) use($pre_content) {
   return $pre_content . $content;
 }, 10, 1);
+
+add_filter('login_message', function($content) use($pre_content) {
+	return $pre_content . $content;
+});
+
+add_action('admin_footer', function() use($pre_content) {
+	?>
+	<style>.plugin-delta-pre-content{position:absolute; margin-left: 20rem;}</style>
+	<?php
+	echo $pre_content;
+});
--- integrations/plugins/plugin-gamma/plugin-gamma.php
+++ integrations/plugins/plugin-gamma/plugin-gamma.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Plugin Gamma
  * Plugin URI:        https://fontawesome.com/
- * Description:       Unregistered Client: tries to enqueue its own v4.7.0 webfont version from cdn.jsdelivr.net. It prepends a block before each blog post that displays an icon with "fa fa-bathtub", which is a version 4 specification that the v4shim should translate to "fas fa-bath".
+ * Description:       Unregistered Client: tries to enqueue its own v4.7.0 webfont version from cdn.jsdelivr.net. On the front end, it prepends a block before each blog post that displays an icon with "fa fa-bathtub", which is a version 4 specification that the v4shim should translate to "fas fa-bath". It adds the same output on the login page, and in the footer of admin pages.
  * Version:           0.0.1
  * Author:            Font Awesome
  * Author URI:        https://fontawesome.com/
@@ -14,20 +14,21 @@ defined( 'WPINC' ) || die;
 define( 'GAMMA_PLUGIN_VERSION', '0.0.1' );
 define( 'GAMMA_PLUGIN_LOG_PREFIX', 'gamma-plugin' );
 
-add_action('wp_enqueue_scripts', function(){
-	print "This rogue output from " . GAMMA_PLUGIN_LOG_PREFIX . " should not be returned by our config controller. If it does, it will break the admin client.";
+foreach( ['wp_enqueue_scripts', 'admin_enqueue_scripts', 'login_enqueue_scripts'] as $action ) {
+	add_action( $action, function () {
+		print "This rogue output from " . GAMMA_PLUGIN_LOG_PREFIX . " should not be returned by our config controller. If it does, it will break the admin client.";
 
-  wp_enqueue_style(
-    'GAMMA_PLUGIN_LOG_PREFIX',
-    'https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css',
-    array(),
-    null,
-    'all'
-  );
+		wp_enqueue_style(
+			'GAMMA_PLUGIN_LOG_PREFIX',
+			'https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css',
+			array(),
+			null,
+			'all'
+		);
 
-  wp_enqueue_style( 'plugin-gamma-style', plugins_url( 'style.css', __FILE__ ) );
-}, 99);
-
+		wp_enqueue_style( 'plugin-gamma-style', plugins_url( 'style.css', __FILE__ ) );
+	}, 99 );
+}
 
 add_action('font_awesome_enqueued', function($loadSpec){
 	error_log(
@@ -40,8 +41,7 @@ add_action('font_awesome_enqueued', function($loadSpec){
 	);
 }, 10, 3);
 
-add_filter('the_content', function($content){
-  $pre_content = <<<EOT
+$pre_content = <<<EOT
 <div class="plugin-gamma-pre-content" style="border: 1px solid grey;">
   <h2>Plugin Gamma</h2>
   <p>v4.7.0 icon name: "fa fa-bathtub": <i class="fa fa-bathtub"></i></p>
@@ -50,5 +50,18 @@ add_filter('the_content', function($content){
   <p class="hand-scissors-icon"><code>:before</code> pseudo-element should match <code>far fa-hand-scissors</code>: <i class="far fa-hand-scissors"></i></p>
 </div>
 EOT;
+
+add_filter('the_content', function($content) use($pre_content){
   return $pre_content . $content;
+});
+
+add_filter('login_message', function($content) use($pre_content) {
+	return $pre_content . $content;
+});
+
+add_action('admin_footer', function() use($pre_content) {
+	?>
+	<style>.plugin-gamma-pre-content{position:absolute; margin-left: 20rem;}</style>
+	<?php
+	echo $pre_content;
 }, 10, 1);
