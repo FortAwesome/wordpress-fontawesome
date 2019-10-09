6ea79a72599987a9e3ac1ad0d2b35f29a9042afe
Mike Wilkerson <mwilkerson@gmail.com>
2019-10-09 07:11:52 -0700

    WIP: rough in the conflict detection controller test suite

---
+++ tests/test-conflict-detection-controller.php
@@ -0,0 +1,62 @@
+<?php
+namespace FortAwesome;
+
+/**
+ * Class ConflictDetectionControllerTest
+ *
+ * @noinspection PhpCSValidationInspection
+ */
+// phpcs:ignoreFile Squiz.Commenting.ClassComment.Missing
+// phpcs:ignoreFile Generic.Commenting.DocComment.MissingShort
+require_once dirname( __FILE__ ) . '/_support/font-awesome-phpunit-util.php';
+
+/**
+ * Class ConflictDetectionControllerTest
+ */
+class ConflictDetectionControllerTest extends \WP_UnitTestCase {
+	protected $server;
+	protected $admin_user;
+	protected $namespaced_route = "/" . FontAwesome::REST_API_NAMESPACE . '/report-conflicts';
+	protected $fa;
+
+	public function setUp() {
+		reset_db();
+		FontAwesome::reset();
+		$this->set_options('5.4.1');
+
+		global $wp_rest_server;
+
+		$this->server = $wp_rest_server = new \WP_REST_Server;
+		$this->admin_user = get_users( [ 'role' => 'administrator' ] )[0];
+
+		wp_set_current_user( $this->admin_user->ID, $this->admin_user->user_login );
+
+		add_action(
+			'rest_api_init',
+			array(
+				new FontAwesome_Conflict_Detection_Controller(
+					FontAwesome::PLUGIN_NAME,
+					FontAwesome::REST_API_NAMESPACE
+				),
+				'register_routes',
+			)
+		);
+
+		do_action( 'rest_api_init' );
+	}
+
+	public function set_options( $version ) {
+		update_option(
+			FontAwesome::OPTIONS_KEY,
+			array_merge(
+				FontAwesome::DEFAULT_USER_OPTIONS,
+				[ 'version' => $version ]
+			)
+		);
+	}
+
+	public function test_register_route() {
+		$routes = $this->server->get_routes();
+		$this->assertArrayHasKey( $this->namespaced_route, $routes );
+  }
+}
