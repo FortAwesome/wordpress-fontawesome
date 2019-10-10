641c6221b86b16d7ad9909b66dccaad489fa88a7
Mike Wilkerson <mwilkerson@gmail.com>
2019-10-10 11:15:25 -0700

    WIP: add conflict detection ignore attrs to most scripts and links
    TODO: make the font face shims an external CSS so we can <link> to
    it as an external and therefore also add the requisite detection ignore
    attr.

--- tests/test-enqueue.php
+++ tests/test-enqueue.php
@@ -74,10 +74,11 @@ class EnqueueTest extends \WP_UnitTestCase {
 	}
 
 	public function assert_webfont($output, $license_subdomain, $version, $refute = false) {
+		$ignore_detection = fa()->detecting_conflicts() ? "data-fa-detection-ignore " : "";
 		$this->assertEquals(
 			$refute ? 0 : 1,
 			preg_match(
-				"/<link[\s]+rel=\'stylesheet\'[\s]+id=\'font-awesome-official-css\'[\s]+href=\'https:\/\/${license_subdomain}\.fontawesome\.com\/releases\/v${version}\/css\/all\.css\'[\s]+type=\'text\/css\'[\s]+media=\'all\'[\s]+integrity=\"sha384-fake123\"[\s]+crossorigin=\"anonymous\"[\s]*\/>/",
+				"/<link[\s]+${ignore_detection}[\s]*rel=\'stylesheet\'[\s]+id=\'font-awesome-official-css\'[\s]+href=\'https:\/\/${license_subdomain}\.fontawesome\.com\/releases\/v${version}\/css\/all\.css\'[\s]+type=\'text\/css\'[\s]+media=\'all\'[\s]+integrity=\"sha384-fake123\"[\s]+crossorigin=\"anonymous\"[\s]*\/>/",
 				$output
 			),
 			self::OUTPUT_MATCH_FAILURE_MESSAGE
@@ -89,10 +90,11 @@ class EnqueueTest extends \WP_UnitTestCase {
 	}
 
 	public function assert_svg($output, $license_subdomain, $version, $refute = false) {
+		$ignore_detection = fa()->detecting_conflicts() ? "data-fa-detection-ignore " : "";
 		$this->assertEquals(
 			$refute ? 0 : 1,
 			preg_match(
-				"/<script[\s]+defer[\s]+crossorigin=\"anonymous\"[\s]+integrity=\"sha384-fake123\"[\s]+type=\'text\/javascript\'[\s]+src=\'https:\/\/${license_subdomain}\.fontawesome\.com\/releases\/v${version}\/js\/all\.js\'><\/script>/",
+				"/<script[\s]+${ignore_detection}[\s]*defer[\s]+crossorigin=\"anonymous\"[\s]+integrity=\"sha384-fake123\"[\s]+type=\'text\/javascript\'[\s]+src=\'https:\/\/${license_subdomain}\.fontawesome\.com\/releases\/v${version}\/js\/all\.js\'><\/script>/",
 				$output
 			),
 			self::OUTPUT_MATCH_FAILURE_MESSAGE
@@ -104,10 +106,11 @@ class EnqueueTest extends \WP_UnitTestCase {
 	}
 
 	public function assert_webfont_v4shim($output, $license_subdomain, $version, $refute = false){
+		$ignore_detection = fa()->detecting_conflicts() ? "data-fa-detection-ignore " : "";
 		$this->assertEquals(
 			$refute ? 0 : 1,
 			preg_match(
-				"/<link[\s]+rel=\'stylesheet\'[\s]+id=\'font-awesome-official-v4shim-css\'[\s]+href=\'https:\/\/${license_subdomain}\.fontawesome\.com\/releases\/v${version}\/css\/v4-shims\.css\'[\s]+type=\'text\/css\'[\s]+media=\'all\'[\s]+integrity=\"sha384-fake246\"[\s]+crossorigin=\"anonymous\"[\s]*\/>/",
+				"/<link[\s]+${ignore_detection}[\s]*rel=\'stylesheet\'[\s]+id=\'font-awesome-official-v4shim-css\'[\s]+href=\'https:\/\/${license_subdomain}\.fontawesome\.com\/releases\/v${version}\/css\/v4-shims\.css\'[\s]+type=\'text\/css\'[\s]+media=\'all\'[\s]+integrity=\"sha384-fake246\"[\s]+crossorigin=\"anonymous\"\s*\/>/",
 				$output
 			),
 			self::OUTPUT_MATCH_FAILURE_MESSAGE
@@ -119,18 +122,30 @@ class EnqueueTest extends \WP_UnitTestCase {
 	}
 
 	public function assert_svg_v4shim($output, $license_subdomain, $version, $refute = false){
+		$ignore_detection = fa()->detecting_conflicts() ? "data-fa-detection-ignore " : "";
 		$this->assertEquals(
 			$refute ? 0 : 1,
 			preg_match(
-				"/<script[\s]+defer[\s]+crossorigin=\"anonymous\"[\s]+integrity=\"sha384-fake246\"[\s]+type=\'text\/javascript\'[\s]+src=\'https:\/\/${license_subdomain}\.fontawesome\.com\/releases\/v${version}\/js\/v4-shims\.js\'><\/script>/",
+				"/<script[\s]+${ignore_detection}[\s]*defer[\s]+crossorigin=\"anonymous\"[\s]+integrity=\"sha384-fake246\"[\s]+type=\'text\/javascript\'[\s]+src=\'https:\/\/${license_subdomain}\.fontawesome\.com\/releases\/v${version}\/js\/v4-shims\.js\'><\/script>/",
 				$output
 			),
-			self::OUTPUT_MATCH_FAILURE_MESSAGE
+			self::OUTPUT_MATCH_FAILURE_MESSAGE . "\n\n$output\n\n"
 		);
 	}
 
 	public function refute_svg_v4shim($output, $license_subdomain, $version){
 		$this->assert_svg_v4shim($output, $license_subdomain, $version, true);
+	}
+
+	public function assert_font_face_inline_style_detection_ignore($output, $refute = false) {
+		$this->assertEquals(
+			$refute ? 0 : 1,
+			preg_match(
+				"/<style[\s]+data-fa-detection-ignore[\s]+id=\'font-awesome-official-v4shim-inline-css\'/",
+				$output
+			),
+			self::OUTPUT_MATCH_FAILURE_MESSAGE
+		);
 	}
 
 	public function assert_font_face_overrides($output, $license_subdomain, $version, $refute = false){
@@ -164,10 +179,11 @@ class EnqueueTest extends \WP_UnitTestCase {
 	}
 
 	public function assert_svg_pseudo_elements($output, $refute = false){
+		$ignore_detection = fa()->detecting_conflicts() ? "data-fa-detection-ignore " : "";
 		$this->assertEquals(
 			$refute ? 0 : 1,
 			preg_match(
-				'/searchPseudoElements:\s*true/',
+				"/<script\s*${ignore_detection}.*?>\s*.*?searchPseudoElements:\s*true/",
 				$output
 			),
 			self::OUTPUT_MATCH_FAILURE_MESSAGE
@@ -311,7 +327,7 @@ class EnqueueTest extends \WP_UnitTestCase {
 		$this->assert_svg_pseudo_elements( $output );
 	}
 
-	public function test_conflict_detector_enqueued_when_enabled() {
+	public function test_conflict_detector_enqueued_when_enabled_svg() {
 		$now = new DateTime('now', new DateTimeZone('UTC'));
 		// ten minutes later
 		$later = $now->add(new DateInterval('PT10M'));
@@ -327,7 +343,7 @@ class EnqueueTest extends \WP_UnitTestCase {
 		);
 
 		$options = wp_parse_args(
-			[ 'version' => '5.1.1' ],
+			[ 'technology' => 'svg', 'version' => '5.1.1', 'v4compat' => true, 'svgPseudoElements' => true ],
 			fa()->options()
 		);
 		$resource_collection = $this->build_mock_resource_collection( $options );
@@ -339,11 +355,51 @@ class EnqueueTest extends \WP_UnitTestCase {
 
 		$this->assertTrue( wp_script_is( FontAwesome::RESOURCE_HANDLE_CONFLICT_DETECTOR, 'enqueued' ) );
 
-		/*
-		$this->refute_webfont( $output, 'pro', $version );
-		$this->assert_svg( $output, 'pro', $version );
-		$this->refute_svg_v4shim( $output, 'pro', $version );
+		$this->assert_svg( $output, 'use', $version );
 		$this->assert_svg_pseudo_elements( $output );
+		$this->assert_svg_v4shim( $output, 'use', $version );
+	}
+
+	public function test_conflict_detector_enqueued_when_enabled_webfont() {
+		$now = new DateTime('now', new DateTimeZone('UTC'));
+		// ten minutes later
+		$later = $now->add(new DateInterval('PT10M'));
+
+		update_option(
+			FontAwesome::OPTIONS_KEY,
+			array_merge(
+				FontAwesome::DEFAULT_USER_OPTIONS,
+				array(
+					'detectConflictsUntil' => $later->format(DateTimeInterface::ATOM)
+				)
+			)
+		);
+
+		$options = wp_parse_args(
+			[ 'version' => '5.1.1', 'v4compat' => true ],
+			fa()->options()
+		);
+		$resource_collection = $this->build_mock_resource_collection( $options );
+		$version = $resource_collection->version();
+
+		fa()->enqueue_cdn( $options, $resource_collection );
+
+		$output = $this->captureOutput();
+
+		$this->assertTrue( wp_script_is( FontAwesome::RESOURCE_HANDLE_CONFLICT_DETECTOR, 'enqueued' ) );
+
+		$this->assert_webfont( $output, 'use', $version );
+		$this->assert_webfont_v4shim( $output, 'use', $version );
+
+	  /**
+		 * TODO: test that every script, style, or link that might be enqueued
+		 * will have the detection ignore attribute on it.
+		 *
+		 * TODO: make sure the conflict detector script itself has the ignore attr, even if the chosen
+		 * technology is webfont.
+		 */
+		/*
+		$this->assert_font_face_inline_style_detection_ignore($output);
 		*/
 	}
 }
