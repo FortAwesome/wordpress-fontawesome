24720b2547279193df0d89ee5148c722bcef963a
Mike Wilkerson <mwilkerson@gmail.com>
2019-10-10 16:08:47 -0700

    WIP: initial implementation of conflict detection reporting JavaScript

--- public_assets/conflict-detection-report.js
+++ public_assets/conflict-detection-report.js
@@ -1,1 +1,47 @@
-console.log("hello from conflict detection reporter");
+function reportDetectedConflicts(params){
+  var nodesTested = params.nodesTested || {}
+  var md5ForFontFaceShimInlineStyle = 'f3350ebe5e9c1693c9864031b56b381f'
+  var apiNonce = window.wpFontAwesomeOfficialConflictReporting['api_nonce'] || null
+  var apiBaseUrl = window.wpFontAwesomeOfficialConflictReporting['api_url'] || null
+  var FETCH = typeof window.fetch == 'function' ? window.fetch : null
+
+  if(! FETCH) {
+    alert("Font Awesome Conflict Detection: not supported for this browser. Please try again with a current version browser.")
+    return
+  }
+
+  if( !apiNonce || !apiBaseUrl ) {
+    alert("Font Awesome Conflict Detection failed because it's not properly configured.")
+    return
+  }
+
+  var promises = []
+
+  Object.keys(nodesTested.conflict).map(function(key){
+    promises.push(
+      FETCH(
+        apiBaseUrl + '/report-conflicts',
+        {
+          headers: {
+            'X-WP-Nonce': apiNonce,
+            'Content-Type': 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify(nodesTested.conflict[key])
+        }
+      )
+    )
+  })
+
+  Promise.all(promises)
+  .then(function(results){
+    alert('done')
+  })
+  .catch(function(error){
+    alert('error')
+  })
+}
+
+window.FontAwesomeDetection = {
+  report: reportDetectedConflicts
+}
