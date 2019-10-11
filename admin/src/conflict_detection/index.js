541ce2ad8578d5883b3a1a67a309d5008232db22
Mike Wilkerson <mwilkerson@gmail.com>
2019-10-11 16:12:44 -0700

    switch to axios and temporarily send statuses to console instead of alerts

--- admin/src/conflict_detection/index.js
+++ admin/src/conflict_detection/index.js
@@ -1,52 +1,46 @@
-function reportDetectedConflicts(params){
-  var nodesTested = params.nodesTested || {}
-  var apiNonce = window.wpFontAwesomeOfficialConflictReporting['api_nonce'] || null
-  var apiBaseUrl = window.wpFontAwesomeOfficialConflictReporting['api_url'] || null
-  var FETCH = typeof window.fetch == 'function' ? window.fetch : null
+import axios from 'axios'
 
-  if(! FETCH) {
-    alert("Font Awesome Conflict Detection: not supported for this browser. Please try again with a current version browser.")
-    return
-  }
+function reportDetectedConflicts({nodesTested = null}){
+  const apiNonce = window.wpFontAwesomeOfficialConflictReporting['api_nonce'] || null
+  const apiBaseUrl = window.wpFontAwesomeOfficialConflictReporting['api_url'] || null
 
   if( !apiNonce || !apiBaseUrl ) {
-    alert("Font Awesome Conflict Detection failed because it's not properly configured.")
+    console.error("Font Awesome Conflict Detection failed because it's not properly configured.")
     return
   }
 
-  var payload = Object.keys(nodesTested.conflict).reduce(function(acc, md5){
+  const payload = Object.keys(nodesTested.conflict).reduce(function(acc, md5){
     acc[md5] = nodesTested.conflict[md5]
     return acc
   }, {})
 
-  var errorMsg = 'Font Awesome Conflict Detection: found ' +
+  const errorMsg = 'Font Awesome Conflict Detection: found ' +
     Object.keys(payload).length + ' conflicts' +
     ' but failed when trying to submit them to your WordPress server. Sorry!'+
     ' You might just try again by reloading this page.';
 
-  FETCH(
+  axios.post(
     apiBaseUrl + '/report-conflicts',
+    payload,
     {
       headers: {
-        'X-WP-Nonce': apiNonce,
-        'Content-Type': 'application/json'
-      },
-      method: 'POST',
-      body: JSON.stringify(payload)
+        'X-WP-Nonce': apiNonce
+      }
     }
   )
   .then(function(response){
-    if( response.ok ) {
-      alert('Font Awesome Conflict Detection: ran successfully and submitted ' +
+    const { status } = response
+    if (204 === status) {
+      console.log('Font Awesome Conflict Detection: ran successfully and submitted ' +
         Object.keys(payload).length +
         ' conflicts to your WordPress server.' +
         ' You can use the Font Awesome plugin settings page to manage them.');
     } else {
-      alert(errorMsg);
-    } 
+      console.error(errorMsg)
+    }
   })
   .catch(function(error){
-    alert(errorMsg);
+    console.error(errorMsg)
   })
 }
 
