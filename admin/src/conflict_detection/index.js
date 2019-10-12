177f9de8eb57da074f3d2c7227f5941a37bfbdce
Mike Wilkerson <mwilkerson@gmail.com>
2019-10-12 11:56:57 -0700

    simplify shadow host style

--- admin/src/conflict_detection/index.js
+++ admin/src/conflict_detection/index.js
@@ -5,7 +5,6 @@ import Reporter from './Reporter'
 
 const conflictDetectionShadowRootElement = document.createElement('DIV')
 conflictDetectionShadowRootElement.setAttribute('id', 'font-awesome-plugin-conflict-detection-shadow-host')
-conflictDetectionShadowRootElement.setAttribute('style', 'position: absolute; top: 0; left: 0; width: 100vw; height: 100vh; contain: content;')
 document.body.appendChild(conflictDetectionShadowRootElement)
 const shadow = conflictDetectionShadowRootElement.attachShadow({ mode: 'open' })
 
