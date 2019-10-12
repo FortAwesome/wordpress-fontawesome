f17a473f8d21a598e0ce12215aec9af4733b811c
Mike Wilkerson <mwilkerson@gmail.com>
2019-10-12 08:29:21 -0700

    WIP: re-work conflict detection reporter in a shadow DOM

--- admin/src/conflict_detection/index.js
+++ admin/src/conflict_detection/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom'
 import ErrorBoundary from '../ErrorBoundary'
 import Reporter from './Reporter'
 
+const conflictDetectionShadowRootElement = document.createElement('DIV')
+conflictDetectionShadowRootElement.setAttribute('id', 'font-awesome-plugin-conflict-detection-shadow-root')
+document.body.appendChild(conflictDetectionShadowRootElement)
+const shadow = conflictDetectionShadowRootElement.attachShadow({ mode: 'open' })
+
 const conflictDetectionContainer = document.createElement('DIV')
 conflictDetectionContainer.setAttribute('class', 'font-awesome-plugin-conflict-detection-container')
 conflictDetectionContainer.setAttribute(
   'style',
-  'position: fixed; right: 10px; bottom: 10px; width: 300px; height: 200px; border: 1px solid lightgrey; background: white; z-index: 99; overflow-y: scroll;'
+  'position: fixed; right: 10px; bottom: 10px; width: 30%; max-width: 80%; height: auto; max-height: 40%; border: 1px solid lightgrey; background: white; z-index: 99; overflow-y: scroll;'
 )
-document.body.appendChild(conflictDetectionContainer)
+shadow.appendChild(conflictDetectionContainer)
 
 ReactDOM.render(<ErrorBoundary><Reporter/></ErrorBoundary>, conflictDetectionContainer)
