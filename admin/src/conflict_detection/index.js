68ce871c19ff4592d5c26d3279adf554a880b234
Mike Wilkerson <mwilkerson@gmail.com>
2019-10-12 06:09:31 -0700

    WIP: fixing conflict detection reporter to lower-right of viewport

--- admin/src/conflict_detection/index.js
+++ admin/src/conflict_detection/index.js
@@ -5,7 +5,10 @@ import Reporter from './Reporter'
 
 const conflictDetectionContainer = document.createElement('DIV')
 conflictDetectionContainer.setAttribute('class', 'font-awesome-plugin-conflict-detection-container')
-conflictDetectionContainer.setAttribute('style', 'position: absolute; right: 100px; bottom: 100px; border: 1px solid black;')
+conflictDetectionContainer.setAttribute(
+  'style',
+  'position: fixed; right: 10px; bottom: 10px; width: 300px; height: 200px; border: 1px solid lightgrey; background: white; z-index: 99;'
+)
 document.body.appendChild(conflictDetectionContainer)
 
 ReactDOM.render(<ErrorBoundary><Reporter/></ErrorBoundary>, conflictDetectionContainer)
