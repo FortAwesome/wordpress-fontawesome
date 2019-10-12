6090f37fb47d320990bdc1cca3b30febf07edd98
Mike Wilkerson <mwilkerson@gmail.com>
2019-10-12 12:05:55 -0700

    manually inject FA css into shadow DOM

--- admin/src/conflict_detection/index.js
+++ admin/src/conflict_detection/index.js
@@ -2,10 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ErrorBoundary from '../ErrorBoundary'
 import Reporter from './Reporter'
+import { dom } from '@fortawesome/fontawesome-svg-core'
 
 const conflictDetectionShadowRootElement = document.createElement('DIV')
 conflictDetectionShadowRootElement.setAttribute('id', 'font-awesome-plugin-conflict-detection-shadow-host')
 document.body.appendChild(conflictDetectionShadowRootElement)
 const shadow = conflictDetectionShadowRootElement.attachShadow({ mode: 'open' })
 
-ReactDOM.render(<ErrorBoundary><Reporter/></ErrorBoundary>, shadow)
+const faStyle = document.createElement('STYLE')
+const css = dom.css()
+const cssText = document.createTextNode(css)
+faStyle.appendChild(cssText)
+
+const shadowContainer = document.createElement('DIV')
+
+shadow.appendChild(faStyle)
+shadow.appendChild(shadowContainer)
+
+ReactDOM.render(<ErrorBoundary><Reporter/></ErrorBoundary>, shadowContainer)
