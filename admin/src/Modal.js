90d30876067acfc7c5cdc7761741284ca52d22e8
Mike Wilkerson <mwilkerson@gmail.com>
2019-10-15 14:01:31 -0700

    remove stray debug output

--- admin/src/Modal.js
+++ admin/src/Modal.js
@@ -15,7 +15,6 @@ class Modal extends React.Component {
 
   handleEscape(e) {
     const { onClose } = this.props
-    console.log(`DEBUG: handleEscape`)
     if(27 === e.keyCode){
       onClose()
     }
