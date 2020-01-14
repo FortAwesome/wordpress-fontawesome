b5f21f07ad5099d6e0b31ca2972141d9ad84fad6
Mike Wilkerson <mwilkerson@gmail.com>
2020-01-14 07:54:54 -0800

    Refine plugin activation logic
    - Create some separation between activate() and initialize() since
      this code, when used as composer library, will need a way to ensure
      that Font Awesome is properly initialized, though that could
      conceivably mean something different from activating. For now,
      activate() just delegates to initialize().
    
    - initialize() respects existing options, only setting up defaults
      if the options are empty, or if forced.

--- includes/class-fontawesome-activator.php
+++ includes/class-fontawesome-activator.php
@@ -12,14 +12,45 @@ require_once trailingslashit( dirname(__FILE__) ) . 'class-fontawesome.php';
 class FontAwesome_Activator {
 
 	/**
-	 * Sets default user options upon plugin activation.
+	 * Initializes plugin options only if they are empty.
 	 *
+	 * @since 4.0.0
 	 * @throws FontAwesome_NoReleasesException
 	 */
 	public static function activate() {
+		self::initialize();
+	}
+
+	/**
+	 * Initializes plugin options with defaults only if they are empty.
+	 * Otherwise, it leaves alone options that are already present.
+	 * 
+	 * Sets default user options. Will attempt to get the latest available version,
+	 * which requires access to the Font Awesome API server. Throws FontAwesome_NoReleasesException
+	 * when that request fails.
+	 * 
+	 * @param bool $force if true, overwrite any existing options with defaults
+	 *
+	 * @since 4.0.0
+	 * @throws FontAwesome_NoReleasesException
+	 */
+	public static function initialize($force = FALSE) {
+		if( $force || ! get_option( FontAwesome::OPTIONS_KEY ) ) {
+			self::initialize_user_options();
+		}
+
+		if( $force || ! get_option( FontAwesome::UNREGISTERED_CLIENTS_OPTIONS_KEY ) ) {
+			self::initialize_unregistered_clients_options();
+		}
+	}
+
+	private static function initialize_user_options() {
 		$version = fa()->get_latest_version();
 		$options = array_merge( FontAwesome::DEFAULT_USER_OPTIONS, [ 'version' => $version ] );
 		update_option( FontAwesome::OPTIONS_KEY, $options );
+	}
+
+	private static function initialize_unregistered_clients_options() {
 		update_option( FontAwesome::UNREGISTERED_CLIENTS_OPTIONS_KEY, array() );
 	}
 }
--- tests/test-activation.php
+++ tests/test-activation.php
@@ -36,4 +36,72 @@ class ActivationTest extends \WP_UnitTestCase {
 			get_option( FontAwesome::UNREGISTERED_CLIENTS_OPTIONS_KEY )
 		);
 	}
+
+	public function test_initialize_from_scratch_creates_default_config() {
+		FontAwesome_Activator::initialize();
+		$actual_options   = get_option( FontAwesome::OPTIONS_KEY );
+		$expected_options = array_merge( FontAwesome::DEFAULT_USER_OPTIONS, [ 'version' => fa()->get_latest_version() ] );
+		$this->assertEquals( $expected_options, $actual_options );
+
+		$this->assertEquals(
+			array(),
+			get_option( FontAwesome::UNREGISTERED_CLIENTS_OPTIONS_KEY )
+		);
+	}
+
+	public function test_initialize_preserves_existing_options() {
+		$expected_options = array_merge( FontAwesome::DEFAULT_USER_OPTIONS, [ 'version' => '5.11.1', 'usePro' => ! FontAwesome::DEFAULT_USER_OPTIONS['usePro'] ] );
+		update_option( FontAwesome::OPTIONS_KEY, $expected_options );
+
+		$expected_unregistered_clients_options = array(
+			'a9a9aa2d454f77cd623d6755c902c408' => array(
+			'type' => 'script',
+			'src'  => 'http://example.com/fake.js'
+			),
+		);
+    	update_option(
+			FontAwesome::UNREGISTERED_CLIENTS_OPTIONS_KEY,
+			$expected_unregistered_clients_options
+		);
+
+		FontAwesome_Activator::initialize();
+		$actual_options = get_option( FontAwesome::OPTIONS_KEY );
+		$actual_unregistered_clients_options = get_option( FontAwesome::UNREGISTERED_CLIENTS_OPTIONS_KEY );
+
+		$this->assertEquals( $expected_options, $actual_options );
+
+		$this->assertEquals(
+			$expected_unregistered_clients_options,
+			$actual_unregistered_clients_options
+		);
+	}
+
+	public function test_initialize_force_overwrites_with_defaults() {
+		$initial_options = array_merge( FontAwesome::DEFAULT_USER_OPTIONS, [ 'version' => '5.11.1', 'usePro' => ! FontAwesome::DEFAULT_USER_OPTIONS['usePro'] ] );
+		update_option( FontAwesome::OPTIONS_KEY, $initial_options );
+
+		$initial_unregistered_clients_options = array(
+			'a9a9aa2d454f77cd623d6755c902c408' => array(
+			'type' => 'script',
+			'src'  => 'http://example.com/fake.js'
+			),
+		);
+    	update_option(
+			FontAwesome::UNREGISTERED_CLIENTS_OPTIONS_KEY,
+			$initial_unregistered_clients_options
+		);
+
+		$expected_options = array_merge( FontAwesome::DEFAULT_USER_OPTIONS, [ 'version' => fa()->get_latest_version() ] );
+
+		FontAwesome_Activator::initialize(TRUE);
+		$actual_options = get_option( FontAwesome::OPTIONS_KEY );
+		$actual_unregistered_clients_options = get_option( FontAwesome::UNREGISTERED_CLIENTS_OPTIONS_KEY );
+
+		$this->assertEquals( $expected_options, $actual_options );
+
+		$this->assertEquals(
+			array(),
+			get_option( FontAwesome::UNREGISTERED_CLIENTS_OPTIONS_KEY )
+		);
+	}
 }
