f6a7b3a2e2769f270761884d17146d96636f7ce4
Mike Wilkerson <mwilkerson@gmail.com>
2020-01-23 07:20:31 -0800

    WIP: begin roughing in a toggle between KitsConfig and CDN Config on SettingsTab

---
+++ admin/src/KitsConfigView.js
@@ -0,0 +1,28 @@
+import React from 'react'
+/*
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  addPendingOption,
+  checkPreferenceConflicts,
+  submitPendingOptions
+} from './store/actions'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faDotCircle,
+  faSpinner,
+  faCheckSquare,
+  faCheck,
+  faSkull,
+  faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+  
+import { faCircle, faSquare } from '@fortawesome/free-regular-svg-icons'
+*/
+//import styles from './KitsConfigView.module.css'
+//import sharedStyles from './App.module.css'
+
+export default function KisConfigView(props) {
+
+  return <div>
+    hello, KitsConfigView
+  </div>
+}
--- admin/src/SettingsTab.js
+++ admin/src/SettingsTab.js
@@ -1,8 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Options from './Options'
+import KitsConfigView from './KitsConfigView'
+import sharedStyles from './App.module.css'
+import optionStyles from './Options.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDotCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCircle } from '@fortawesome/free-regular-svg-icons'
+import classnames from 'classnames'
+import styles from './SettingsTab.module.css'
 
 export default function SettingsTab() {
+  const [useKits, setUseKits] = useState(true)
+
   return <div>
-    <Options />
+    <div className={ styles['select-use-kits-container'] }>
+      <input
+        id="select_use_kits"
+        name="select_use_kits"
+        type="radio"
+        value={ useKits }
+        checked={ useKits }
+        onChange={ () => setUseKits(! useKits) }
+        className={ classnames(sharedStyles['sr-only'], sharedStyles['input-radio-custom']) }
+      />
+      <label htmlFor="select_use_kits" className={ optionStyles['option-label'] }>
+        <span className={ sharedStyles['relative'] }>
+          <FontAwesomeIcon
+            icon={ faDotCircle }
+            className={ sharedStyles['checked-icon'] }
+            size="lg"
+            fixedWidth
+          />
+          <FontAwesomeIcon
+            icon={ faCircle }
+            className={ sharedStyles['unchecked-icon'] }
+            size="lg"
+            fixedWidth
+          />
+        </span>
+        <span className={ optionStyles['option-label-text'] }>
+        Use A Kit
+        </span>
+      </label>
+    </div>
+    {
+      useKits
+      ? <KitsConfigView />
+      : null
+    }
+    <div className={ styles['select-use-cdn-container'] }>
+      <input
+        id="select_use_cdn"
+        name="select_use_cdn"
+        type="radio"
+        value={ ! useKits }
+        checked={ ! useKits }
+        onChange={ () => setUseKits(! useKits) }
+        className={ classnames(sharedStyles['sr-only'], sharedStyles['input-radio-custom']) }
+      />
+      <label htmlFor="select_use_cdn" className={ optionStyles['option-label'] }>
+        <span className={ sharedStyles['relative'] }>
+          <FontAwesomeIcon
+            icon={ faDotCircle }
+            className={ sharedStyles['checked-icon'] }
+            size="lg"
+            fixedWidth
+          />
+          <FontAwesomeIcon
+            icon={ faCircle }
+            className={ sharedStyles['unchecked-icon'] }
+            size="lg"
+            fixedWidth
+          />
+        </span>
+        <span className={ optionStyles['option-label-text'] }>
+        Use CDN
+        </span>
+      </label>
+    </div>
+    {
+      useKits
+      ? null
+      : <Options />
+    }
   </div>
 }
