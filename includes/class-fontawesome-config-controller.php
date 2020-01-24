260d9e9be611969b4af1337ec38fc6ae8f00c8da
Mike Wilkerson <mwilkerson@gmail.com>
2020-01-24 10:09:56 -0800

    Handle options updates while ensuring validity of options schema with kits vs non-kits configs

--- includes/class-fontawesome-config-controller.php
+++ includes/class-fontawesome-config-controller.php
@@ -107,24 +107,11 @@ if ( ! class_exists( 'FortAwesome\FontAwesome_Config_Controller' ) ) :
 					}
 				}
 
-				$db_item = array_merge(
-					array(),
-					$body,
-					// Override any literal apiToken with just a boolean value
-					// indicating its presence
-					array( 
-						'options' => array(
-							'apiToken' => boolval( $api_token )
-						)
-					),
-				);
+				$db_item = $this->prepare_item_for_database( $body, boolval( $api_token ) );
 
 				$result = update_option(
 					FontAwesome::OPTIONS_KEY,
-					array_merge(
-						FontAwesome::DEFAULT_USER_OPTIONS,
-						$db_item['options']
-					)
+					$db_item
 				);
 
 				if ( $result ) {
@@ -150,6 +137,81 @@ if ( ! class_exists( 'FortAwesome\FontAwesome_Config_Controller' ) ) :
 					)
 				);
 			}
+		}
+
+		/**
+		 * Filters the incoming data, determines what should actually
+		 * be stored in the database, and ensures that it's valid.
+		 *
+		 * @internal
+		 * @ignore
+		 * @param array $body the request body as returned by get_json_params()
+		 * @throws FontAwesome_ConfigurationException when options indicate a non-kits
+		 *   CDN configuration, but no valid version is present.
+		 */
+		protected function prepare_item_for_database( $body ) {
+			// start with a copy of the defaults and just override them indivually
+			$item = array_merge( array(), FontAwesome::DEFAULT_USER_OPTIONS );
+			$given_options = isset( $body['options'] ) ? $body['options'] : [];
+
+			// Most of the options are handled in the same way:
+			// if they are provided at all, we just use that to override the default
+			if ( isset( $given_options['usePro'] ) ) {
+				$item['usePro'] = $given_options['usePro'];
+			}
+			if ( isset( $given_options['v4compat'] ) ) {
+				$item['v4compat'] = $given_options['v4compat'];
+			}
+			if ( isset( $given_options['technology'] ) ) {
+				$item['technology'] = $given_options['technology'];
+			}
+			if ( isset( $given_options['svgPseudoElements'] ) ) {
+				$item['svgPseudoElements'] = $given_options['svgPseudoElements'];
+			}
+			if ( isset( $given_options['detectConflictsUntil'] ) ) {
+				$item['detectConflictsUntil'] = $given_options['detectConflictsUntil'];
+			}
+			if ( isset( $given_options['kitToken'] ) ) {
+				$item['kitToken'] = $given_options['kitToken'];
+			}
+			if ( isset( $given_options['usePro'] ) ) {
+				$item['usePro'] = $given_options['usePro'];
+			}
+			if ( isset( $given_options['blocklist'] ) ) {
+				$item['blocklist'] = $given_options['blocklist'];
+			}
+
+			// The apiToken is handled specially.
+			// We only store a boolean value indicating whether and apiToken
+			// has been stored. It's the responsibility of the calling code
+			// to store the actual API Token appropriately.
+			if ( isset( $given_options['apiToken'] ) ) {
+				$item['apiToken'] = boolval( $given_options['apiToken'] );
+			}
+
+			// The version is handle specially.
+			// 1. An valid version number but be used, if any version is present at all.
+			//    The string 'latest' is not valid here. It's need to be something like 5.12.0.
+			//
+			// 2. If the options are configured to use a kit, then version is not
+			//    required, because the version comes from the kit itself. Otherwise,
+			//    we must have a version. It's configured to use a kit if the kitToken
+			//    is some string value.
+			//
+			// 3. To ensure that switching back and forth between kits and non-kits
+			//    configurations is smooth, if we do switch a kits config here, we'll
+			//    keep the last-used version.
+			if (! isset( $given_options['kitToken'] ) || ! is_string( $given_options['kitToken'] ) ) {
+				if ( isset( $given_options['version'] ) && 1 === preg_match('/[0-9]+\.[0-9]+/', $given_options['version'] ) ) {
+					$item['version'] = $given_options['version'];
+				} else {
+					throw new FontAwesome_ConfigurationException('valid version expected but not given');
+				}
+			} else {
+				$item['version'] = fa()->version();
+			}
+
+			return $item;
 		}
 
 		/**
