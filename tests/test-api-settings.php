56a7fb0ab13d45dba2681b2dd2cba9b61e2821ff
Mike Wilkerson <mwilkerson@gmail.com>
2020-01-25 18:54:39 -0800

    auto-formatting

--- tests/test-api-settings.php
+++ tests/test-api-settings.php
@@ -1,5 +1,6 @@
 <?php
 namespace FortAwesome;
+
 /**
  * Class ApiSettingsTest
  */
@@ -12,7 +13,7 @@ use \WP_Error, \InvalidArgumentException;
 class ApiSettingsTest extends \WP_UnitTestCase {
 
 	public function setUp() {
-		wp_delete_file(FontAwesome_API_Settings::ini_path());
+		wp_delete_file( FontAwesome_API_Settings::ini_path() );
 		FontAwesome_Activator::activate();
 	}
 
@@ -34,20 +35,20 @@ class ApiSettingsTest extends \WP_UnitTestCase {
 api_token = "abc123"
 access_token = "xyz456"
 access_token_expiration_time = 999999999
-EOD;		
-		$path = FontAwesome_API_Settings::ini_path();
-		$this->assertStringEndsWith(FontAwesome_API_Settings::FILENAME, $path);
+EOD;
+		$path     = FontAwesome_API_Settings::ini_path();
+		$this->assertStringEndsWith( FontAwesome_API_Settings::FILENAME, $path );
 
-		$write_result = @file_put_contents( $path, $contents ); 
+		$write_result = @file_put_contents( $path, $contents );
 
-		$this->assertGreaterThan(0, $write_result, 'writing ini file failed');
+		$this->assertGreaterThan( 0, $write_result, 'writing ini file failed' );
 
 		// Force re-read
 		$api_settings = FontAwesome_API_Settings::reset();
 
-		$this->assertEquals('abc123', $api_settings->api_token());
-		$this->assertEquals('xyz456', $api_settings->access_token());
-		$this->assertEquals(999999999, $api_settings->access_token_expiration_time());
+		$this->assertEquals( 'abc123', $api_settings->api_token() );
+		$this->assertEquals( 'xyz456', $api_settings->access_token() );
+		$this->assertEquals( 999999999, $api_settings->access_token_expiration_time() );
 
 	}
 
@@ -55,9 +56,9 @@ EOD;
 		// Force re-read
 		$api_settings = FontAwesome_API_Settings::reset();
 
-		$this->assertNull($api_settings->api_token());
-		$this->assertNull($api_settings->access_token());
-		$this->assertNull($api_settings->access_token_expiration_time());
+		$this->assertNull( $api_settings->api_token() );
+		$this->assertNull( $api_settings->access_token() );
+		$this->assertNull( $api_settings->access_token_expiration_time() );
 	}
 
 	public function test_write() {
@@ -65,30 +66,29 @@ EOD;
 		// Force re-read
 		$api_settings = FontAwesome_API_Settings::reset();
 
-		$api_settings->set_api_token('foo');
-		$api_settings->set_access_token('bar');
-		$api_settings->set_access_token_expiration_time(42);
+		$api_settings->set_api_token( 'foo' );
+		$api_settings->set_access_token( 'bar' );
+		$api_settings->set_access_token_expiration_time( 42 );
 
 		$result = $api_settings->write();
-		$this->assertTrue($result, 'writing ini file failed');
+		$this->assertTrue( $result, 'writing ini file failed' );
 
 		// Force re-read
 		$api_settings = FontAwesome_API_Settings::reset();
 
-		$this->assertEquals('foo', $api_settings->api_token());
-		$this->assertEquals('bar', $api_settings->access_token());
-		$this->assertEquals(42, $api_settings->access_token_expiration_time());
+		$this->assertEquals( 'foo', $api_settings->api_token() );
+		$this->assertEquals( 'bar', $api_settings->access_token() );
+		$this->assertEquals( 42, $api_settings->access_token_expiration_time() );
 		$this->assertTrue( is_int( $api_settings->access_token_expiration_time() ) );
 
 		// Round-trip it again
-
 		$result = $api_settings->write();
 		// Force re-read
 		$api_settings = FontAwesome_API_Settings::reset();
 
-		$this->assertEquals('foo', $api_settings->api_token());
-		$this->assertEquals('bar', $api_settings->access_token());
-		$this->assertEquals(42, $api_settings->access_token_expiration_time());
+		$this->assertEquals( 'foo', $api_settings->api_token() );
+		$this->assertEquals( 'bar', $api_settings->access_token() );
+		$this->assertEquals( 42, $api_settings->access_token_expiration_time() );
 		$this->assertTrue( is_int( $api_settings->access_token_expiration_time() ) );
 	}
 
@@ -99,17 +99,17 @@ EOD;
 		// Force re-read
 		$api_settings = FontAwesome_API_Settings::reset();
 
-		$api_settings->set_api_token('foo');
+		$api_settings->set_api_token( 'foo' );
 
 		$result = $api_settings->write();
-		$this->assertTrue($result, 'writing ini file failed');
+		$this->assertTrue( $result, 'writing ini file failed' );
 
 		// Force re-read
 		$api_settings = FontAwesome_API_Settings::reset();
 
-		$this->assertEquals('foo', $api_settings->api_token());
-		$this->assertNull($api_settings->access_token());
-		$this->assertNull($api_settings->access_token_expiration_time());
+		$this->assertEquals( 'foo', $api_settings->api_token() );
+		$this->assertNull( $api_settings->access_token() );
+		$this->assertNull( $api_settings->access_token_expiration_time() );
 	}
 
 	public function test_request_access_token() {
@@ -122,27 +122,27 @@ EOD;
 				'body'     => json_encode(
 					array(
 						'access_token' => '123',
-						'expires_in' => 3600
+						'expires_in'   => 3600,
 					)
 				),
-			) 
-		); 
+			)
+		);
 
-		$api_settings->set_api_token('xyz');
+		$api_settings->set_api_token( 'xyz' );
 		$result = $api_settings->request_access_token();
 
-		$this->assertTrue($result);
-		$this->assertEquals('123', $api_settings->access_token());
-		$this->assertEquals('xyz', $api_settings->api_token());
-		$this->assertEqualsWithDelta(time() + 3600, $api_settings->access_token_expiration_time(), 2.0);
+		$this->assertTrue( $result );
+		$this->assertEquals( '123', $api_settings->access_token() );
+		$this->assertEquals( 'xyz', $api_settings->api_token() );
+		$this->assertEqualsWithDelta( time() + 3600, $api_settings->access_token_expiration_time(), 2.0 );
 
 		// Force re-read
 		$api_settings = FontAwesome_API_Settings::reset();
 
 		// Everything should have remained the same through the write/read round trip
-		$this->assertEquals('123', $api_settings->access_token());
-		$this->assertEquals('xyz', $api_settings->api_token());
-		$this->assertEqualsWithDelta(time() + 3600, $api_settings->access_token_expiration_time(), 2.0);
+		$this->assertEquals( '123', $api_settings->access_token() );
+		$this->assertEquals( 'xyz', $api_settings->api_token() );
+		$this->assertEqualsWithDelta( time() + 3600, $api_settings->access_token_expiration_time(), 2.0 );
 	}
 
 	public function test_request_access_token_without_api_token() {
@@ -155,11 +155,11 @@ EOD;
 				'body'     => json_encode(
 					array(
 						'access_token' => '123',
-						'expires_in' => 3600
+						'expires_in'   => 3600,
 					)
 				),
-			) 
-		); 
+			)
+		);
 
 		$result = $api_settings->request_access_token();
 
@@ -170,9 +170,9 @@ EOD;
 	public function test_request_access_token_when_request_errors() {
 		$api_settings = $this->create_api_settings_with_mocked_response(
 			new WP_Error()
-		); 
+		);
 
-		$api_settings->set_api_token('xyz');
+		$api_settings->set_api_token( 'xyz' );
 
 		$result = $api_settings->request_access_token();
 
@@ -188,12 +188,11 @@ EOD;
 					'code'    => 403,
 					'message' => 'Forbidden',
 				),
-				'body'     => ''
-			) 
-		); 
+				'body'     => '',
+			)
+		);
 
-
-		$api_settings->set_api_token('xyz');
+		$api_settings->set_api_token( 'xyz' );
 
 		$result = $api_settings->request_access_token();
 
@@ -211,14 +210,13 @@ EOD;
 				),
 				'body'     => json_encode(
 					array(
-						'expires_in' => 3600
+						'expires_in' => 3600,
 					)
-				)
-			) 
-		); 
+				),
+			)
+		);
 
-
-		$api_settings->set_api_token('xyz');
+		$api_settings->set_api_token( 'xyz' );
 
 		$result = $api_settings->request_access_token();
 
@@ -236,14 +234,13 @@ EOD;
 				),
 				'body'     => json_encode(
 					array(
-						'access_token' => 'abc'
+						'access_token' => 'abc',
 					)
-				)
-			) 
-		); 
+				),
+			)
+		);
 
-
-		$api_settings->set_api_token('xyz');
+		$api_settings->set_api_token( 'xyz' );
 
 		$result = $api_settings->request_access_token();
 
@@ -257,7 +254,7 @@ EOD;
 
 		$this->expectException( InvalidArgumentException::class );
 
-		$api_settings->set_access_token_expiration_time("abc");
+		$api_settings->set_access_token_expiration_time( 'abc' );
 	}
 
 	public function test_set_access_token_expiration_time_given_zero() {
@@ -265,7 +262,7 @@ EOD;
 
 		$this->expectException( InvalidArgumentException::class );
 
-		$api_settings->set_access_token_expiration_time(0);
+		$api_settings->set_access_token_expiration_time( 0 );
 	}
 
 	public function test_reset() {
@@ -273,20 +270,20 @@ EOD;
 api_token = "abc123"
 access_token = "xyz456"
 access_token_expiration_time = 999999999
-EOD;		
-		$path = FontAwesome_API_Settings::ini_path();
-		$this->assertStringEndsWith(FontAwesome_API_Settings::FILENAME, $path);
+EOD;
+		$path     = FontAwesome_API_Settings::ini_path();
+		$this->assertStringEndsWith( FontAwesome_API_Settings::FILENAME, $path );
 
-		$write_result = @file_put_contents( $path, $contents ); 
+		$write_result = @file_put_contents( $path, $contents );
 
-		$this->assertGreaterThan(0, $write_result, 'writing ini file failed');
+		$this->assertGreaterThan( 0, $write_result, 'writing ini file failed' );
 
 		// Force re-read
 		$api_settings = FontAwesome_API_Settings::reset();
 
-		$this->assertEquals('abc123', $api_settings->api_token());
-		$this->assertEquals('xyz456', $api_settings->access_token());
-		$this->assertEquals(999999999, $api_settings->access_token_expiration_time());
+		$this->assertEquals( 'abc123', $api_settings->api_token() );
+		$this->assertEquals( 'xyz456', $api_settings->access_token() );
+		$this->assertEquals( 999999999, $api_settings->access_token_expiration_time() );
 	}
 
 	public function test_remove() {
@@ -294,30 +291,30 @@ EOD;
 api_token = "abc123"
 access_token = "xyz456"
 access_token_expiration_time = 999999999
-EOD;		
-		$path = FontAwesome_API_Settings::ini_path();
-		$this->assertStringEndsWith(FontAwesome_API_Settings::FILENAME, $path);
+EOD;
+		$path     = FontAwesome_API_Settings::ini_path();
+		$this->assertStringEndsWith( FontAwesome_API_Settings::FILENAME, $path );
 
-		$write_result = @file_put_contents( $path, $contents ); 
-		$this->assertGreaterThan(0, $write_result, 'writing ini file failed');
+		$write_result = @file_put_contents( $path, $contents );
+		$this->assertGreaterThan( 0, $write_result, 'writing ini file failed' );
 
 		$this->assertTrue( file_exists( FontAwesome_API_Settings::ini_path() ) );
 
 		// Force re-read
 		$api_settings = FontAwesome_API_Settings::reset();
 
-		$this->assertEquals('abc123', $api_settings->api_token());
-		$this->assertEquals('xyz456', $api_settings->access_token());
-		$this->assertEquals(999999999, $api_settings->access_token_expiration_time());
+		$this->assertEquals( 'abc123', $api_settings->api_token() );
+		$this->assertEquals( 'xyz456', $api_settings->access_token() );
+		$this->assertEquals( 999999999, $api_settings->access_token_expiration_time() );
 
 		// Now remove it, expecting both the in-memory and on-disk data to be cleared.
 		$api_settings->remove();
 
 		// Force re-read again
 		$api_settings = FontAwesome_API_Settings::reset();
-		$this->assertNull($api_settings->api_token());
-		$this->assertNull($api_settings->access_token());
-		$this->assertNull($api_settings->access_token_expiration_time());
+		$this->assertNull( $api_settings->api_token() );
+		$this->assertNull( $api_settings->access_token() );
+		$this->assertNull( $api_settings->access_token_expiration_time() );
 
 		$this->assertFalse( file_exists( FontAwesome_API_Settings::ini_path() ) );
 	}
