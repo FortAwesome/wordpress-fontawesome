da15ce4e7baf0beb56dc990a8b6095df063e9e09
Mike Wilkerson <mwilkerson@gmail.com>
2020-01-25 19:10:56 -0800

    auto-formatting

--- includes/class-fontawesome-metadata-provider.php
+++ includes/class-fontawesome-metadata-provider.php
@@ -25,7 +25,7 @@ class FontAwesome_Metadata_Provider {
 	/**
 	 * @ignore
 	 */
-  protected static $instance = null;
+	protected static $instance = null;
 
   // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 	/**
@@ -33,7 +33,7 @@ class FontAwesome_Metadata_Provider {
 	 */
 	protected function post( $url, $args = array() ) {
 		return wp_remote_post( $url, $args );
-  }
+	}
 
 	/**
 	 * Returns the FontAwesome_Metadata_Provider singleton instance.
@@ -55,16 +55,16 @@ class FontAwesome_Metadata_Provider {
 	 * @ignore
 	 */
 	protected function query_errors( $errors ) {
-		$error_string = "";
+		$error_string = '';
 
-		foreach ($errors as $error) {
+		foreach ( $errors as $error ) {
 			$error_string .= $error->message;
 		}
 
 		return $error_string;
 	}
 
-  /**
+	/**
 	 * Returns an associative array indicating the status of the status of the last network
 	 * request that attempted to retrieve releases metadata, or null if no network request has
 	 * been issued during the life time of the current Singleton instance.
@@ -95,8 +95,8 @@ class FontAwesome_Metadata_Provider {
 	 * @ignore
 	 */
 	public function get_available_versions() {
-		$query = 'query {versions}';
-		$json = $this->metadata_query($query);
+		$query         = 'query {versions}';
+		$json          = $this->metadata_query( $query );
 		$version_array = array();
 
 		if ( 200 !== $this->status['code'] ) {
@@ -105,27 +105,27 @@ class FontAwesome_Metadata_Provider {
 
 		try {
 			$versions = $json->versions;
-      foreach($versions as $key => $val) {
-        array_push( $version_array, strval( $val ) );
-      }
+			foreach ( $versions as $key => $val ) {
+				array_push( $version_array, strval( $val ) );
+			}
 
-      usort(
-        $versions,
-        function( $first, $second ) {
-          return version_compare( $second, $first );
-        }
-      );
+			usort(
+				$versions,
+				function( $first, $second ) {
+					return version_compare( $second, $first );
+				}
+			);
 
 			return $versions;
 		} catch ( Exception $e ) {
 			return array(
-					'code'    => 0,
-					'message' => 'Whoops, we failed to fetch the versions.',
+				'code'    => 0,
+				'message' => 'Whoops, we failed to fetch the versions.',
 			);
 		} catch ( Error $e ) {
 			return array(
-					'code'    => 0,
-					'message' => 'Whoops, we failed when trying to fetch the versions.',
+				'code'    => 0,
+				'message' => 'Whoops, we failed when trying to fetch the versions.',
 			);
 		}
 	}
@@ -137,20 +137,20 @@ class FontAwesome_Metadata_Provider {
 	 *
 	 * @ignore
 	 */
-	public function metadata_query($query_string) {
+	public function metadata_query( $query_string ) {
 		$init_status = array(
 			'code'    => null,
 			'message' => '',
 		);
 
 		$args = array(
-			'method' => 'POST',
+			'method'  => 'POST',
 			'headers' => array(
-				'Content-Type' => 'application/json'
+				'Content-Type' => 'application/json',
 			),
-			'body' => '{"query": ' . json_encode($query_string) . '}'
+			'body'    => '{"query": ' . json_encode( $query_string ) . '}',
 		);
-		$url = FONTAWESOME_API_URL;
+		$url  = FONTAWESOME_API_URL;
 
 		try {
 			$response = $this->post( $url, $args );
@@ -172,11 +172,11 @@ class FontAwesome_Metadata_Provider {
 			}
 
 			$body_contents = $response['body'];
-			$json_body = json_decode( $body_contents );
+			$json_body     = json_decode( $body_contents );
 
-			if (property_exists($json_body, 'errors')) {
+			if ( property_exists( $json_body, 'errors' ) ) {
 				return array(
-					'code' => 200,
+					'code'    => 200,
 					'message' => $this->query_errors( $json_body->errors ),
 				);
 			}
