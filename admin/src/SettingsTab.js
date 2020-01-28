b56171c7efe8032ed9d6c7f7d54ada678f0d5c84
Mike Wilkerson <mwilkerson@gmail.com>
2020-01-28 06:52:44 -0800

    re-flow the settings tab

--- admin/src/SettingsTab.js
+++ admin/src/SettingsTab.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import Options from './Options'
 import KitsConfigView from './KitsConfigView'
 import sharedStyles from './App.module.css'
@@ -10,77 +11,75 @@ import classnames from 'classnames'
 import styles from './SettingsTab.module.css'
 
 export default function SettingsTab() {
-  const [useKits, setUseKits] = useState(true)
+  const alreadyUsingKit = useSelector( state => !!state.options.kitToken )
+  const [useKits, setUseKits] = useState(alreadyUsingKit)
 
   return <div>
-    <div className={ styles['select-use-kits-container'] }>
-      <input
-        id="select_use_kits"
-        name="select_use_kits"
-        type="radio"
-        value={ useKits }
-        checked={ useKits }
-        onChange={ () => setUseKits(! useKits) }
-        className={ classnames(sharedStyles['sr-only'], sharedStyles['input-radio-custom']) }
-      />
-      <label htmlFor="select_use_kits" className={ optionStyles['option-label'] }>
-        <span className={ sharedStyles['relative'] }>
-          <FontAwesomeIcon
-            icon={ faDotCircle }
-            className={ sharedStyles['checked-icon'] }
-            size="lg"
-            fixedWidth
-          />
-          <FontAwesomeIcon
-            icon={ faCircle }
-            className={ sharedStyles['unchecked-icon'] }
-            size="lg"
-            fixedWidth
-          />
-        </span>
-        <span className={ optionStyles['option-label-text'] }>
-        Use A Kit
-        </span>
-      </label>
+    <div className={ styles['select-config-container'] }>
+      <div>
+        <input
+          id="select_use_kits"
+          name="select_use_kits"
+          type="radio"
+          value={ useKits }
+          checked={ useKits }
+          onChange={ () => setUseKits(! useKits) }
+          className={ classnames(sharedStyles['sr-only'], sharedStyles['input-radio-custom']) }
+        />
+        <label htmlFor="select_use_kits" className={ optionStyles['option-label'] }>
+          <span className={ sharedStyles['relative'] }>
+            <FontAwesomeIcon
+              icon={ faDotCircle }
+              className={ sharedStyles['checked-icon'] }
+              size="lg"
+              fixedWidth
+            />
+            <FontAwesomeIcon
+              icon={ faCircle }
+              className={ sharedStyles['unchecked-icon'] }
+              size="lg"
+              fixedWidth
+            />
+          </span>
+          <span className={ optionStyles['option-label-text'] }>
+          Use A Kit
+          </span>
+        </label>
+      </div>
+      <div>
+        <input
+          id="select_use_cdn"
+          name="select_use_cdn"
+          type="radio"
+          value={ ! useKits }
+          checked={ ! useKits }
+          onChange={ () => setUseKits(! useKits) }
+          className={ classnames(sharedStyles['sr-only'], sharedStyles['input-radio-custom']) }
+        />
+        <label htmlFor="select_use_cdn" className={ optionStyles['option-label'] }>
+          <span className={ sharedStyles['relative'] }>
+            <FontAwesomeIcon
+              icon={ faDotCircle }
+              className={ sharedStyles['checked-icon'] }
+              size="lg"
+              fixedWidth
+            />
+            <FontAwesomeIcon
+              icon={ faCircle }
+              className={ sharedStyles['unchecked-icon'] }
+              size="lg"
+              fixedWidth
+            />
+          </span>
+          <span className={ optionStyles['option-label-text'] }>
+          Use CDN
+          </span>
+        </label>
+      </div>
     </div>
     {
       useKits
       ? <KitsConfigView />
-      : null
-    }
-    <div className={ styles['select-use-cdn-container'] }>
-      <input
-        id="select_use_cdn"
-        name="select_use_cdn"
-        type="radio"
-        value={ ! useKits }
-        checked={ ! useKits }
-        onChange={ () => setUseKits(! useKits) }
-        className={ classnames(sharedStyles['sr-only'], sharedStyles['input-radio-custom']) }
-      />
-      <label htmlFor="select_use_cdn" className={ optionStyles['option-label'] }>
-        <span className={ sharedStyles['relative'] }>
-          <FontAwesomeIcon
-            icon={ faDotCircle }
-            className={ sharedStyles['checked-icon'] }
-            size="lg"
-            fixedWidth
-          />
-          <FontAwesomeIcon
-            icon={ faCircle }
-            className={ sharedStyles['unchecked-icon'] }
-            size="lg"
-            fixedWidth
-          />
-        </span>
-        <span className={ optionStyles['option-label-text'] }>
-        Use CDN
-        </span>
-      </label>
-    </div>
-    {
-      useKits
-      ? null
       : <Options />
     }
   </div>
