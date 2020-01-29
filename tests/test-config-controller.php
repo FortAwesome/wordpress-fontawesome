5362155b775a34078f2100240bf12958d79bda79
Mike Wilkerson <mwilkerson@gmail.com>
2020-01-29 14:24:43 -0800

    Tighten up various cases for ConfigController update_item()
    Expand test coverage

--- tests/test-config-controller.php
+++ tests/test-config-controller.php
@@ -78,4 +78,368 @@ class ConfigControllerTest extends \WP_UnitTestCase {
 			}
 		}
 	}
+
+	/**
+	 * When we update the options to use a kit, and that kit's version is not
+	 * 'latest', but an concrete version number like '5.4.1', then that 5.4.1
+	 * is what should be stored.
+	 */
+	public function test_update_with_kit_not_latest_version() {
+		// Start with the version being something else.
+		$this->set_version( '5.3.1' );
+
+		$request_body = array(
+				'options' => array(
+					'usePro' => true,
+					'v4compat' => true,
+					'technology' => 'webfont',
+					'svgPseudoElements' => false,
+					'detectConflictsUntil' => 0,
+					'blocklist' => [],
+					'kitToken' => '778ccf8260',
+					'apiToken' => true,
+					'version' => '5.4.1',
+				),
+				'conflicts' => array()
+			);
+
+		$request  = new \WP_REST_Request(
+			'PUT',
+			$this->namespaced_route
+		);
+
+    	$request->add_header('Content-Type', 'application/json');
+    	$request->set_body( wp_json_encode( $request_body ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 200, $response->get_status() );
+		$data = $response->get_data();
+		$this->assertArrayHasKey( 'conflicts', $data );
+		$this->assertArrayHasKey( 'options', $data );
+		$this->assertEquals( '5.4.1', $data['options']['version']);
+		$this->assertEquals( '5.4.1', fa()->version() );
+	}
+
+	public function test_update_with_kit_invalid_version() {
+		// Start with the version being something else.
+		$this->set_version( '5.3.1' );
+
+		$request_body = array(
+				'options' => array(
+					'usePro' => true,
+					'v4compat' => true,
+					'technology' => 'webfont',
+					'svgPseudoElements' => false,
+					'detectConflictsUntil' => 0,
+					'blocklist' => [],
+					'kitToken' => '778ccf8260',
+					'apiToken' => true,
+					'version' => 'cat.dog',
+				),
+				'conflicts' => array()
+			);
+
+		$request  = new \WP_REST_Request(
+			'PUT',
+			$this->namespaced_route
+		);
+
+    	$request->add_header('Content-Type', 'application/json');
+    	$request->set_body( wp_json_encode( $request_body ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 400, $response->get_status() );
+		// Version unchanged
+		$this->assertEquals( '5.3.1', fa()->version() );
+		$data = $response->get_data();
+		$this->assertArrayHasKey( 'code', $data );
+		$this->assertEquals( 'fontawesome_config', $data['code'] );
+	}
+
+	public function test_update_with_nonkit_invalid_version() {
+		// Start with the version being something else.
+		$this->set_version( '5.3.1' );
+
+		$request_body = array(
+				'options' => array(
+					'usePro' => true,
+					'v4compat' => true,
+					'technology' => 'webfont',
+					'svgPseudoElements' => false,
+					'detectConflictsUntil' => 0,
+					'blocklist' => [],
+					'kitToken' => null,
+					'apiToken' => false,
+					'version' => 'cat.dog',
+				),
+				'conflicts' => array()
+			);
+
+		$request  = new \WP_REST_Request(
+			'PUT',
+			$this->namespaced_route
+		);
+
+    	$request->add_header('Content-Type', 'application/json');
+    	$request->set_body( wp_json_encode( $request_body ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 400, $response->get_status() );
+		// Version unchanged
+		$this->assertEquals( '5.3.1', fa()->version() );
+		$data = $response->get_data();
+		$this->assertArrayHasKey( 'code', $data );
+		$this->assertEquals( 'fontawesome_config', $data['code'] );
+	}
+
+	public function test_update_with_nonkit_symbolic_latest_version() {
+		// Start with the version being something else.
+		$this->set_version( '5.3.1' );
+
+		$request_body = array(
+				'options' => array(
+					'usePro' => true,
+					'v4compat' => true,
+					'technology' => 'webfont',
+					'svgPseudoElements' => false,
+					'detectConflictsUntil' => 0,
+					'blocklist' => [],
+					'kitToken' => null,
+					'apiToken' => false,
+					'version' => 'latest',
+				),
+				'conflicts' => array()
+			);
+
+		$request  = new \WP_REST_Request(
+			'PUT',
+			$this->namespaced_route
+		);
+
+    	$request->add_header('Content-Type', 'application/json');
+    	$request->set_body( wp_json_encode( $request_body ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 400, $response->get_status() );
+		// Version unchanged
+		$this->assertEquals( '5.3.1', fa()->version() );
+		$data = $response->get_data();
+		$this->assertArrayHasKey( 'code', $data );
+		$this->assertEquals( 'fontawesome_config', $data['code'] );
+	}
+
+	public function test_update_with_nonkit_valid_version() {
+		// Start with the version being something else.
+		$this->set_version( '5.3.1' );
+
+		$request_body = array(
+				'options' => array(
+					'usePro' => true,
+					'v4compat' => true,
+					'technology' => 'webfont',
+					'svgPseudoElements' => false,
+					'detectConflictsUntil' => 0,
+					'blocklist' => [],
+					'kitToken' => null,
+					'apiToken' => false,
+					'version' => '5.4.1',
+				),
+				'conflicts' => array()
+			);
+
+		$request  = new \WP_REST_Request(
+			'PUT',
+			$this->namespaced_route
+		);
+
+    	$request->add_header('Content-Type', 'application/json');
+    	$request->set_body( wp_json_encode( $request_body ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 200, $response->get_status() );
+		$data = $response->get_data();
+		$this->assertArrayHasKey( 'conflicts', $data );
+		$this->assertArrayHasKey( 'options', $data );
+		$this->assertEquals( '5.4.1', $data['options']['version']);
+		$this->assertEquals( '5.4.1', fa()->version() );
+	}
+
+	public function test_update_with_no_options_in_body() {
+		// Start with the version being something else.
+		$this->set_version( '5.3.1' );
+
+		$request_body = array();
+
+		$request  = new \WP_REST_Request(
+			'PUT',
+			$this->namespaced_route
+		);
+
+    	$request->add_header('Content-Type', 'application/json');
+    	$request->set_body( wp_json_encode( $request_body ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 400, $response->get_status() );
+		$data = $response->get_data();
+		$this->assertArrayHasKey( 'code', $data );
+		$this->assertEquals( 'fontawesome_config', $data['code'] );
+		// Version unchanged
+		$this->assertEquals( '5.3.1', fa()->version() );
+	}
+
+	public function test_update_with_valid_cdn_config() {
+		// Start with the version being something else.
+		$this->set_version( '5.3.1' );
+
+		$detect_conflicts_until = time() + 10;
+
+		$request_body = array(
+				'options' => array(
+					'usePro' => true,
+					'v4compat' => true,
+					'technology' => 'svg',
+					'svgPseudoElements' => true,
+					'detectConflictsUntil' => $detect_conflicts_until,
+					'blocklist' => ['abc', 'efg'],
+					'kitToken' => null,
+					'apiToken' => false,
+					'version' => '5.4.1',
+				),
+				'conflicts' => array()
+			);
+
+		$request  = new \WP_REST_Request(
+			'PUT',
+			$this->namespaced_route
+		);
+
+    	$request->add_header('Content-Type', 'application/json');
+    	$request->set_body( wp_json_encode( $request_body ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 200, $response->get_status() );
+		$data = $response->get_data();
+		$this->assertArrayHasKey( 'conflicts', $data );
+		$this->assertArrayHasKey( 'options', $data );
+		$this->assertEquals( '5.4.1', $data['options']['version']);
+		$this->assertEquals( '5.4.1', fa()->version() );
+
+		$this->assertTrue( $data['options']['usePro'] );
+		$this->assertTrue( fa()->pro() );
+		
+		$this->assertTrue( $data['options']['v4compat'] );
+		$this->assertTrue( fa()->v4_compatibility() );
+
+		$this->assertEquals( 'svg', $data['options']['technology'] );
+		$this->assertEquals( 'svg', fa()->technology() );
+
+		$this->assertTrue( $data['options']['svgPseudoElements'] );
+		$this->assertTrue( fa()->svg_pseudo_elements() );
+
+		$this->assertEquals( $detect_conflicts_until, $data['options']['detectConflictsUntil'] );
+		$this->assertTrue( fa()->detecting_conflicts() );
+
+		$this->assertEquals( ['abc', 'efg'], fa()->blocklist() );
+		$this->assertEquals( ['abc', 'efg'], $data['options']['blocklist'] );
+
+		$this->assertEquals( null, $data['options']['kitToken'] );
+		$this->assertEquals( 'svg', fa()->technology() );
+
+		$this->assertFalse( $data['options']['apiToken'] );
+		$this->assertFalse( fa()->options()['apiToken'] );
+	}
+
+	public function test_update_with_valid_kit_config() {
+		// Start with the version being something else.
+		$this->set_version( '5.3.1' );
+
+		$detect_conflicts_until = time() + 10;
+
+		$request_body = array(
+				'options' => array(
+					'usePro' => true,
+					'v4compat' => true,
+					'technology' => 'svg',
+					'svgPseudoElements' => true,
+					'detectConflictsUntil' => $detect_conflicts_until,
+					'blocklist' => ['abc', 'efg'],
+					'kitToken' => 'abc123',
+					'apiToken' => true,
+					'version' => '5.4.1',
+				),
+				'conflicts' => array()
+			);
+
+		$request  = new \WP_REST_Request(
+			'PUT',
+			$this->namespaced_route
+		);
+
+    	$request->add_header('Content-Type', 'application/json');
+    	$request->set_body( wp_json_encode( $request_body ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 200, $response->get_status() );
+		$data = $response->get_data();
+		$this->assertArrayHasKey( 'conflicts', $data );
+		$this->assertArrayHasKey( 'options', $data );
+		$this->assertEquals( '5.4.1', $data['options']['version']);
+		$this->assertEquals( '5.4.1', fa()->version() );
+
+		$this->assertTrue( $data['options']['usePro'] );
+		$this->assertTrue( fa()->pro() );
+
+		$this->assertTrue( $data['options']['v4compat'] );
+		$this->assertTrue( fa()->v4_compatibility() );
+
+		$this->assertEquals( 'svg', $data['options']['technology'] );
+		$this->assertEquals( 'svg', fa()->technology() );
+
+		$this->assertTrue( $data['options']['svgPseudoElements'] );
+		$this->assertTrue( fa()->svg_pseudo_elements() );
+
+		$this->assertEquals( $detect_conflicts_until, $data['options']['detectConflictsUntil'] );
+		$this->assertTrue( fa()->detecting_conflicts() );
+
+		$this->assertEquals( ['abc', 'efg'], fa()->blocklist() );
+		$this->assertEquals( ['abc', 'efg'], $data['options']['blocklist'] );
+
+		$this->assertEquals( 'abc123', $data['options']['kitToken'] );
+		$this->assertEquals( 'abc123', $data['options']['kitToken'] );
+
+		$this->assertEquals( 'svg', fa()->technology() );
+
+		$this->assertTrue( $data['options']['apiToken'] );
+		$this->assertTrue( fa()->options()['apiToken'] );
+	}
+
+	public function test_fail_when_kit_config_has_no_api_token() {
+		// Start with the version being something else.
+		$this->set_version( '5.3.1' );
+
+		$detect_conflicts_until = time() + 10;
+
+		$request_body = array(
+				'options' => array(
+					'usePro' => true,
+					'v4compat' => true,
+					'technology' => 'svg',
+					'svgPseudoElements' => true,
+					'detectConflictsUntil' => $detect_conflicts_until,
+					'blocklist' => [],
+					'kitToken' => 'abc123',
+					'apiToken' => false,
+					'version' => '5.4.1',
+				),
+				'conflicts' => array()
+			);
+
+		$request  = new \WP_REST_Request(
+			'PUT',
+			$this->namespaced_route
+		);
+
+    	$request->add_header('Content-Type', 'application/json');
+    	$request->set_body( wp_json_encode( $request_body ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 400, $response->get_status() );
+		$data = $response->get_data();
+
+		$this->assertArrayHasKey( 'code', $data );
+		$this->assertEquals( 'fontawesome_config', $data['code'] );
+		// Version unchanged
+		$this->assertEquals( '5.3.1', fa()->version() );
+	}
 }
