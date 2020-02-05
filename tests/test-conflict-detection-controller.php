97e2287a19e58955d8cd020c2cd34512e4278e34
Mike Wilkerson <mwilkerson@gmail.com>
2020-02-05 13:43:38 -0800

    add negative testing for update_detect_conflicts_until

--- tests/test-conflict-detection-controller.php
+++ tests/test-conflict-detection-controller.php
@@ -371,4 +371,57 @@ class ConflictDetectionControllerTest extends \WP_UnitTestCase {
 			fa()->unregistered_clients()
 		);
 	}
+
+	public function test_update_detect_conflicts_until_when_bad_schema() {
+		$now = time();
+		// ten minutes later
+		$later = $now + (10 * 60);
+
+		$initial_unregistered_clients = array(
+			'a9a9aa2d454f77cd623d6755c902c408' => array(
+				'type' => 'script',
+				'src'  => 'http://example.com/fake.js'
+			),
+		);
+
+		update_option(
+			FontAwesome::CONFLICT_DETECTION_OPTIONS_KEY,
+			array(
+				'detectConflictsUntil' => $later,
+				'unregisteredClients'  => $initial_unregistered_clients
+			)
+		);
+
+		$request = new \WP_REST_Request(
+			'PUT',
+			$this->namespaced_detect_until_route
+		);
+
+		$request->add_header('Content-Type', 'application/json');
+
+		$body = array(
+			'detectConflictsUntil' => 'foo'
+		);
+
+		$request->set_body( wp_json_encode( $body ) );
+
+		$response = $this->server->dispatch( $request );
+
+		$this->assertEquals( 400, $response->get_status() );
+
+		$error = $response->as_error();
+
+		$this->assertEquals(
+			'fontawesome_detect_conflicts_until_schema',
+			$error->get_error_code()
+		);
+
+		$this->assertTrue( fa()->detecting_conflicts() );
+
+		// There should have been no change in this.
+		$this->assertEquals(
+			$initial_unregistered_clients,
+			fa()->unregistered_clients()
+		);
+	}
 }
