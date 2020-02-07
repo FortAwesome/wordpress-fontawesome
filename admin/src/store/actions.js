8a645ede0d4850a1160cc9aa269a6da0f2cddd5a
Mike Wilkerson <mwilkerson@gmail.com>
2020-02-07 10:17:59 -0800

    WIP: select unregistered clients for removal

--- admin/src/store/actions.js
+++ admin/src/store/actions.js
@@ -45,6 +45,7 @@ export function updatePendingUnregisteredClientsForDeletion(data = []) {
 
 export function submitPendingUnregisteredClientDeletions() {
   return function(dispatch){
+    console.log('DEBUG: submitPendingUnregisteredClientDeletions')
   }
 }
 
