0cf250c6de69daedcfc4561a6b95f391b48cddc5
Mike Wilkerson <mwilkerson@gmail.com>
2020-02-10 09:54:15 -0800

    WIP: add a KitConfigView with a placeholder view of selected kit properties

--- admin/src/KitConfigView.js
+++ admin/src/KitConfigView.js
@@ -1,8 +1,41 @@
 import React from 'react'
 import styles from './KitSelectView.module.css'
+import PropTypes from 'prop-types'
+import { useSelector } from 'react-redux'
 
-export default function KitConfigView() {
+export default function KitConfigView({ kitToken }) {
+  const kit = useSelector(state => (state.kits || []).find(k => k.token === kitToken))
+
+  if(!kit) {
+    throw new Error('We could not find a kit for the given kitToken. Try reloading this page.')
+  }
+
+  const {
+    licenseSelected,
+    name,
+    shimEnabled,
+    technologySelected,
+    version
+    // The following kit properties are also available, though they do not
+    // have corollaries to the CDN-based configuration alternative.
+
+    /*
+    useIntegrityHash,
+    autoAccessibilityEnabled,
+    integrityHash,
+    minified,
+    */
+  } = kit
+
   return <div className={ styles['kit-config-view-container'] }>
-    KitConfigView placeholder
+    <p>name: { name }</p>
+    <p>version: { version }</p>
+    <p>licenseSelected: { licenseSelected }</p>
+    <p>technologySelected: { technologySelected }</p>
+    <p>shimEnabled (V4 Compatibility): { shimEnabled ? 'true' : 'false' }</p>
   </div>
+}
+
+KitConfigView.propTypes = {
+  kitToken: PropTypes.string.isRequired
 }
