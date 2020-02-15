b1adc0921b6938a7cb1b9f7088f9d63ab44de54b
Mike Wilkerson <mwilkerson@gmail.com>
2020-02-14 16:00:50 -0800

    refine some JSX to satisfy React

--- admin/src/KitConfigView.js
+++ admin/src/KitConfigView.js
@@ -5,8 +5,6 @@ import {
   faExclamationTriangle
  } from '@fortawesome/free-solid-svg-icons'
 import styles from './KitSelectView.module.css'
-import sharedStyles from './App.module.css'
-import classnames from 'classnames'
 import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
 import get from 'lodash/get'
@@ -45,26 +43,30 @@ export default function KitConfigView({ kitToken }) {
       : kitTokenApiData.version
   )
 
+  // TODO: wire up preference conflict markup like this.
+  // <div class="Alert_alert__9rB-8 Alert_alert-warning__a4JcD" role="alert"><div class="Alert_alert-icon__33P-T"><FontAwesomeIcon icon={ faExclamationTriangle } title='warning' fixedWidth /></div><div class="Alert_alert-message__1QY5M"><h2 class="Alert_alert-title__p2H1b">Preference Conflict</h2><div><div>This setting might cause problems for the theme or plugin: eta-plugin.</div></div></div></div>
+
   return <div className={ styles['kit-config-view-container'] }>
     <table className={ styles['selected-kit-settings'] }>
-      <tr>
-        <th className={ styles['label'] }>Icons</th>
-        <td className={ styles['value'] }>{ usePro ? 'Pro' : 'Free' }
-        <div class="Alert_alert__9rB-8 Alert_alert-warning__a4JcD" role="alert"><div class="Alert_alert-icon__33P-T"><FontAwesomeIcon icon={ faExclamationTriangle } title='warning' fixedWidth /></div><div class="Alert_alert-message__1QY5M"><h2 class="Alert_alert-title__p2H1b">Preference Conflict</h2><div><div>This setting might cause problems for the theme or plugin: eta-plugin.</div></div></div></div>
-        </td>
-      </tr>
-      <tr>
-        <th className={ styles['label'] }>Technology</th>
-        <td className={ styles['value'] }>{ technology }</td>
-      </tr>
-      <tr>
-        <th className={ styles['label'] }>Version</th>
-        <td className={ styles['value'] }>{ version }</td>
-      </tr>
-      <tr>
-        <th className={ styles['label'] }>Version 4 Compatability</th>
-        <td className={ styles['value'] }>{ v4Compat ? 'On' : 'Off' }</td>
-      </tr>
+      <tbody>
+        <tr>
+          <th className={ styles['label'] }>Icons</th>
+          <td className={ styles['value'] }>{ usePro ? 'Pro' : 'Free' }
+          </td>
+        </tr>
+        <tr>
+          <th className={ styles['label'] }>Technology</th>
+          <td className={ styles['value'] }>{ technology }</td>
+        </tr>
+        <tr>
+          <th className={ styles['label'] }>Version</th>
+          <td className={ styles['value'] }>{ version }</td>
+        </tr>
+        <tr>
+          <th className={ styles['label'] }>Version 4 Compatability</th>
+          <td className={ styles['value'] }>{ v4Compat ? 'On' : 'Off' }</td>
+        </tr>
+      </tbody>
     </table>
     <p className={ styles['tip-text'] }>Make changes on <a target="_blank" href="https://fontawesome.com/kits">fontawesome.com <FontAwesomeIcon icon={faExternalLinkAlt} /></a> and then refresh.</p>
   </div>
