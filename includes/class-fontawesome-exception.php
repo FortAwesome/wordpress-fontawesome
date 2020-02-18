375569275c58fe427485e4415785a8dcfd436286
Mike Wilkerson <mwilkerson@gmail.com>
2020-02-18 15:57:20 -0800

    WIP: re-work exceptions module for internationalization

--- includes/class-fontawesome-exception.php
+++ includes/class-fontawesome-exception.php
@@ -6,12 +6,6 @@ use \Exception;
 // phpcs:disable Generic.Files.OneClassPerFile.MultipleFound
 
 abstract class FontAwesome_Exception extends Exception {
-
-	/**
-	 * A message appropriate for display to a user.
-	 */
-	protected $ui_message = null;
-
 	/**
 	 * A WP_Error object that was the occassion for this exception.
 	 */
@@ -22,16 +16,6 @@ abstract class FontAwesome_Exception extends Exception {
 	 * Array keys should be like an array that would be returned from wp_remote_post().
 	 */
 	protected $wp_response = null;
-
-	public function __construct( $message = "", $code = 0, $previous = NULL ) {
-		parent::__construct( $message, $code, $previous );
-
-		if( is_string( $message ) && !empty( $message ) ) {
-			$this->message = $message;
-		} elseif( ! is_null( $this->ui_message ) ) {
-			$this->message = $this->ui_message;
-		}
-	}
 
 	public static function with_wp_error( $wp_error ) {
 		// This is how we invoke the derived class's constructor from an inherited static method.
@@ -92,86 +76,210 @@ abstract class FontAwesome_ServerException extends FontAwesome_Exception {}
 abstract class FontAwesome_ClientException extends FontAwesome_Exception {}
 
 class ApiTokenMissingException extends FontAwesome_ClientException {
-	public $ui_message = 'Whoops, it looks like you have not provided a ' .
-		'Font Awesome API Token. Enter one on the Font Awesome plugin settings page.';
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				'Whoops, it looks like you have not provided a ' .
+				'Font Awesome API Token. Enter one on the Font Awesome plugin settings page.',
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ApiTokenEndpointRequestException extends FontAwesome_ServerException {
-	public $ui_message = 'Your WordPress server failed when trying to communicate ' .
-		'with the Font Awesome API token endpoint.';
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				'Your WordPress server failed when trying to communicate ' .
+				'with the Font Awesome API token endpoint.',
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ApiTokenInvalidException extends FontAwesome_ClientException {
-	public $ui_message = 'Whoops, it looks like that API Token is not valid. Try another one?';
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				'Whoops, it looks like that API Token is not valid. Try another one?',
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ApiTokenEndpointResponseException extends FontAwesome_ServerException {
-	public $ui_message = 'Oh no! It looks like your API Token was valid, ' .
-		'but the Font Awesome API server still returned an invalid response.';
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				'Oh no! It looks like your API Token was valid, ' .
+				'but the Font Awesome API server still returned an invalid response.',
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class AccessTokenStorageException extends FontAwesome_ServerException {
-	public $ui_message = 'There was a problem trying to store API credentials. Your API Token ' .
-		' was valid, but storage failed.';
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				'There was a problem trying to store API credentials. Your API Token ' .
+				'was valid, but storage failed.',
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ConfigSchemaException extends FontAwesome_ClientException {
-	public function __construct( $code = NULL ) {
-		switch( $code ) {
-			case 'kit_token_no_api_token':
-				return parent::__construct(
-					'A kitToken was given without a valid apiToken'
-				);
-			case 'concrete_version_expected':
-				return parent::__construct(
-					'A Font Awesome version number was expected but not given',
-				);
-			case 'webfont_v4compat_introduced_later':
-				return parent::__construct(
-					'Whoops! You found a corner case here. ' .
-					'Version 4 compatibility for our webfont technology was not introduced until Font Awesome 5.1.0. ' .
-					'Try using a newer version, disabling version 4 compatibility, or switch to SVG.'
-				);
-			default:
-				return parent::__construct(
-					'Invalid options were given'
-				);
-		}
+
+	public static function kit_token_no_api_token() {
+		return new static(
+			esc_html__(
+				'A kitToken was given without a valid apiToken',
+				FONTAWESOME_TEXT_DOMAIN
+			)
+		);
+	}
+
+	public static function concrete_version_expected() {
+		return new static(
+			esc_html__(
+				'A Font Awesome version number was expected but not given',
+				FONTAWESOME_TEXT_DOMAIN
+			)
+		);
+	}
+
+	public static function webfont_v4compat_introduced_later() {
+		return new static(
+			esc_html__(
+				'Whoops! You found a corner case here. ' .
+				'Version 4 compatibility for our webfont technology was not introduced until Font Awesome 5.1.0. ' .
+				'Try using a newer version, disabling version 4 compatibility, or switch to SVG.',
+				FONTAWESOME_TEXT_DOMAIN
+			)
+		);
 	}
 }
 
 class PreferenceRegistrationException extends FontAwesome_ServerException {
-	public $ui_message = 'A theme or plugin registered with Font Awesome threw an exception.';
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				'A theme or plugin registered with Font Awesome threw an exception.',
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ApiRequestException extends FontAwesome_ServerException {
-	public $ui_message = 'Your WordPress server failed trying to send a request to the Font Awesome API server.';
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				'Your WordPress server failed trying to send a request to the Font Awesome API server.',
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ApiResponseException extends FontAwesome_ServerException {
-	public $ui_message = 'An unexpected response was received from the Font Awesome API server.';
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				'An unexpected response was received from the Font Awesome API server.',
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ReleaseProviderStorageException extends FontAwesome_ServerException {
-	public $ui_message = 'Failed to store releases transient';
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				'Failed to store Font Awesome releases metadata in your WordPress datbase.',
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ReleaseMetadataMissingException extends FontAwesome_ServerException {
-	public $ui_message = "Somehow, we're missing metadata about available Font Awesome releaes, which should have " .
-		"already been queried from the Font Awesome API server. Try deactivating and re-activating the Font Awesome plugin.";
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				"Somehow, we're missing metadata about available Font Awesome releaes, which should have " .
+				"already been queried from the Font Awesome API server. Try deactivating and re-activating the Font Awesome plugin.",
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ConfigCorruptionException extends FontAwesome_ServerException {
-	public $ui_message = "When trying to load Font Awesome, the plugin's configuration was invalid. " .
-		"Try deactivating, uninstalling, and re-activating the Font Awesome plugin.";
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				"When trying to load Font Awesome, the plugin's configuration was invalid. " .
+				"Try deactivating, uninstalling, and re-activating the Font Awesome plugin.",
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ConflictDetectionSchemaException extends FontAwesome_ClientException {
-	public $ui_message = "The conflict detection data sent to your WordPress server was invalid.";
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				"The conflict detection data sent to your WordPress server was invalid.",
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 class ConflictDetectionStorageException extends FontAwesome_ServerException {
-	public $ui_message = "We were not able to save conflict detection data to your WordPress database.";
+	public function __construct( $message = null, $code = 0, $previous = null ) {
+		return parent::__construct(
+			esc_html__(
+				"We were not able to save conflict detection data to your WordPress database.",
+				FONTAWESOME_TEXT_DOMAIN
+			),
+			$code,
+			$previous
+		);
+	}
 }
 
 /**
