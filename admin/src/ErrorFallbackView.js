c349f7987a5ac074fdf746491a6ec6bd0f9a54b8
Mike Wilkerson <mwilkerson@gmail.com>
2020-02-25 07:41:27 -0800

    simplify the ErrorFallbackView

--- admin/src/ErrorFallbackView.js
+++ admin/src/ErrorFallbackView.js
@@ -1,59 +1,16 @@
 import React from 'react'
 import styles from './ErrorFallbackView.module.css'
 import Alert from './Alert'
-import PropTypes from 'prop-types'
-import get from 'lodash/get'
 
-function ErrorFallbackView(props) {
-  const { error = {} } = props
-  const clientSideErrorMessage = get(error, 'message', null)
-  const serverErrorMessage = get(error, ['response', 'data', 'message'], null)
-  const serverStackTrace = get(error, ['response', 'data', 'data', 'trace'], null)
-
+function ErrorFallbackView() {
   return <div className={ styles['error-fallback'] }>
     <Alert title='Whoops, this is embarrassing' type='warning'>
       <p>
-        Some unexpected error has occurred.
+        Some unexpected error has occurred. There might be some additional
+        diagnostic information in the JavaScript console.
       </p>
-      {
-        clientSideErrorMessage &&
-        <p>
-          { clientSideErrorMessage }
-        </p>
-      }
-      <p>
-        You might be able to find a solution to this
-        problem in the <a href="https://wordpress.org/support/plugin/font-awesome/" rel="noopener noreferrer" target="_blank">plugin's support forum</a>.
-        If not, you could post about the problem there.
-      </p>
-      {
-       serverErrorMessage  &&
-        <div>
-          The server says:
-          <div className={ styles['additional-message'] }>
-            { serverErrorMessage }
-          </div>
-          {
-            serverStackTrace && <p>A server stack trace is shown below.</p>
-          }
-          <p>Please include this information if you submit an error report.</p>
-        </div>
-      }
     </Alert>
-    {
-      serverStackTrace &&
-      <p>
-        <h2>Server Stack Trace</h2>
-        <pre>
-        { serverStackTrace }
-        </pre>
-      </p>
-    }
   </div>
-}
-
-ErrorFallbackView.propTypes = {
-  message: PropTypes.string
 }
 
 export default ErrorFallbackView
