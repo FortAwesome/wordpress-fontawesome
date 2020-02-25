e3a24bba3469035e91c006cad3905c356b0c19f3
Mike Wilkerson <mwilkerson@gmail.com>
2020-02-25 08:01:00 -0800

    i18n for ErrorFallbackView

--- admin/src/ErrorFallbackView.js
+++ admin/src/ErrorFallbackView.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import styles from './ErrorFallbackView.module.css'
 import Alert from './Alert'
+import { __ } from '@wordpress/i18n'
 
 function ErrorFallbackView() {
   return <div className={ styles['error-fallback'] }>
-    <Alert title='Whoops, this is embarrassing' type='warning'>
+    <Alert title={ __( 'Whoops, this is embarrassing', 'font-awesome' ) } type='warning'>
       <p>
-        Some unexpected error has occurred. There might be some additional
-        diagnostic information in the JavaScript console.
+        {
+          __( 'Some unexpected error has occurred. There might be some additional diagnostic information in the JavaScript console.', 'font-awesome' )
+        }
       </p>
     </Alert>
   </div>
