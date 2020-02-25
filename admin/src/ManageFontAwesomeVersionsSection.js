c4923cca3c0b3c7d23c1551d294b6d2e7c4610bf
Mike Wilkerson <mwilkerson@gmail.com>
2020-02-25 14:40:46 -0800

    i18n ManageFontAwesomeVersionsSection

--- admin/src/ManageFontAwesomeVersionsSection.js
+++ admin/src/ManageFontAwesomeVersionsSection.js
@@ -3,16 +3,15 @@ import styles from './ManageFontAwesomeVersionsSection.module.css'
 import sharedStyles from './App.module.css'
 import ClientPreferencesView from './ClientPreferencesView'
 import classnames from 'classnames'
+import { __ } from '@wordpress/i18n'
 
 export default function ManageFontAwesomeVersionsSection() {
   return <div className={ classnames(sharedStyles['explanation'], styles['font-awesome-versions-section']) }>
-    <h2 className={ sharedStyles['section-title'] }>Versions of Font Awesome Active on Your Site</h2>
+    <h2 className={ sharedStyles['section-title'] }>{ __( 'Versions of Font Awesome Active on Your Site', 'font-awesome' ) }</h2>
     <p>
-    <b>Registered plugins and themes</b> have opted to share information about the
-    Font Awesome settings they are expecting, and are therefore easier to fix.
-    For the <b>unregistered plugins and themes</b>, which are more unpredictable, we
-    have provided options for you to block their Font Awesome source from loading and
-    causing issues.
+      {
+        __( 'Registered plugins and themes have opted to share information about the Font Awesome settings they are expecting, and are therefore easier to fix. For the unregistered plugins and themes, which are more unpredictable, we have provided options for you to block their Font Awesome source from loading and causing issues.', 'font-awesome' )
+      }
     </p>
     <ClientPreferencesView />
   </div>
