9eea02196873d917b842ba4d24a0efa58dba9f9b
Mike Wilkerson <mwilkerson@gmail.com>
2020-02-26 08:25:27 -0800

    re-introduce inline <b> to copy in ManageFontAwesomeVersionsSection using experimental feature

--- admin/src/ManageFontAwesomeVersionsSection.js
+++ admin/src/ManageFontAwesomeVersionsSection.js
@@ -3,14 +3,22 @@ import styles from './ManageFontAwesomeVersionsSection.module.css'
 import sharedStyles from './App.module.css'
 import ClientPreferencesView from './ClientPreferencesView'
 import classnames from 'classnames'
-import { __ } from '@wordpress/i18n'
+import { __, sprintf } from '@wordpress/i18n'
+import { __experimentalCreateInterpolateElement } from '@wordpress/element'
 
 export default function ManageFontAwesomeVersionsSection() {
   return <div className={ classnames(sharedStyles['explanation'], styles['font-awesome-versions-section']) }>
     <h2 className={ sharedStyles['section-title'] }>{ __( 'Versions of Font Awesome Active on Your Site', 'font-awesome' ) }</h2>
     <p>
       {
-        __( 'Registered plugins and themes have opted to share information about the Font Awesome settings they are expecting, and are therefore easier to fix. For the unregistered plugins and themes, which are more unpredictable, we have provided options for you to block their Font Awesome source from loading and causing issues.', 'font-awesome' )
+        __experimentalCreateInterpolateElement(
+          sprintf(
+            __( '<b>Registered plugins and themes</b> have opted to share information about the Font Awesome settings they are expecting, and are therefore easier to fix. For the <b>unregistered plugins and themes</b>, which are more unpredictable, we have provided options for you to block their Font Awesome source from loading and causing issues.', 'font-awesome' )
+          ),
+          {
+            b: <b />
+          }
+        )
       }
     </p>
     <ClientPreferencesView />
