aeed6538d26506764aee1bef3af04333a0f4a01e
Mike Wilkerson <mwilkerson@gmail.com>
2020-03-03 17:57:20 -0800

    Update PreferenceCheckController to return new REST responses

--- includes/class-fontawesome-preference-check-controller.php
+++ includes/class-fontawesome-preference-check-controller.php
@@ -2,6 +2,7 @@
 namespace FortAwesome;
 
 require_once trailingslashit( FONTAWESOME_DIR_PATH ) . 'includes/class-fontawesome-exception.php';
+require_once trailingslashit( FONTAWESOME_DIR_PATH ) . 'includes/class-fontawesome-rest-response.php';
 
 use \WP_REST_Controller, \WP_REST_Response, \WP_Error, \Error, \Exception;
 
@@ -72,15 +73,15 @@ class FontAwesome_Preference_Check_Controller extends WP_REST_Controller {
 
 			$conflicts = fa()->conflicts_by_option( $request->get_json_params() );
 
-			return new WP_REST_Response( $conflicts, 200 );
+			return new FontAwesome_REST_Response( $conflicts, 200 );
 		} catch ( FontAwesome_ServerException $e ) {
-			return fa_500( $e );
+			return new FontAwesome_REST_Response( wpe_fontawesome_server_exception( $e ), 500 );
 		} catch ( FontAwesome_Exception $e ) {
-			return fa_400( $e );
+			return new FontAwesome_REST_Response( wpe_fontawesome_client_exception( $e ), 400 );
 		} catch ( Exception $e ) {
-			return unknown_error_500( $e );
+			return new FontAwesome_REST_Response( wpe_fontawesome_unknown_error( $e ), 500 );
 		} catch ( Error $e ) {
-			return unknown_error_500( $e );
+			return new FontAwesome_REST_Response( wpe_fontawesome_unknown_error( $e ), 500 );
 		}
 	}
 }
