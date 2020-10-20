f497e64a9211171ffb94b6a64e99d74af56ce0e0
first-wgrt <55440658+first-wgrt@users.noreply.github.com>
2020-10-20 08:53:25 +0200

    Update class-fontawesome.php
    Changes as proposed in #92

--- includes/class-fontawesome.php
+++ includes/class-fontawesome.php
@@ -758,25 +758,16 @@ class FontAwesome {
 			);
 		}
 
-		$icon_data = 'data:image/svg+xml;base64,'
-			// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
-			. base64_encode(
-				'<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 448 512"><path fill="'
-				. $this->get_admin_icon_color()
-				. '" d="M397.8 32H50.2C22.7 32 0 54.7 0 82.2v347.6C0 457.3 22.7 480 50.2 480h347.6c27.5 0 50.2-22.7 50.2-50.2V82.2c0-27.5-22.7-50.2-50.2-50.2zm-45.4 284.3c0 4.2-3.6 6-7.8 7.8-16.7 7.2-34.6 13.7-53.8 13.7-26.9 0-39.4-16.7-71.7-16.7-23.3 0-47.8 8.4-67.5 17.3-1.2.6-2.4.6-3.6 1.2V385c0 1.8 0 3.6-.6 4.8v1.2c-2.4 8.4-10.2 14.3-19.1 14.3-11.3 0-20.3-9-20.3-20.3V166.4c-7.8-6-13.1-15.5-13.1-26.3 0-18.5 14.9-33.5 33.5-33.5 18.5 0 33.5 14.9 33.5 33.5 0 10.8-4.8 20.3-13.1 26.3v18.5c1.8-.6 3.6-1.2 5.4-2.4 18.5-7.8 40.6-14.3 61.5-14.3 22.7 0 40.6 6 60.9 13.7 4.2 1.8 8.4 2.4 13.1 2.4 22.7 0 47.8-16.1 53.8-16.1 4.8 0 9 3.6 9 7.8v140.3z"/></svg>'
-			);
-
 		$admin_menu_command = new FontAwesome_Command(
-			function() use ( $icon_data ) {
-				fa()->screen_id = add_menu_page(
-					/* translators: add_menu_page page_title */
+			function() {
+				fa()->screen_id = add_options_page(
+				    /* translators: add_options_page page_title */
 					esc_html__( 'Font Awesome Settings', 'font-awesome' ),
-					/* translators: add_menu_page menu_title */
+					/* translators: add_options_page menu_title */
 					esc_html__( 'Font Awesome', 'font-awesome' ),
 					'manage_options',
 					self::OPTIONS_PAGE,
-					array( fa(), 'create_admin_page' ),
-					$icon_data
+					array( fa(), 'create_admin_page' )
 				);
 			}
 		);
@@ -2655,38 +2646,6 @@ EOT;
 		}
 
 		return $asset_url_base . $asset_manifest[ $asset ];
-	}
-
-	/**
-	 * Internal use only, not part of this plugin's public API.
-	 *
-	 * @internal
-	 * @ignore
-	 */
-	private function get_admin_icon_color() {
-		// Adapted from wp_color_scheme_settings in WP Core.
-		global $_wp_admin_css_colors;
-
-		$color_scheme = get_user_option( 'admin_color' );
-
-		if ( empty( $_wp_admin_css_colors[ $color_scheme ] ) ) {
-			$color_scheme = 'fresh';
-		}
-
-		if ( ! empty( $_wp_admin_css_colors[ $color_scheme ]->icon_colors ) ) {
-			$icon_colors = $_wp_admin_css_colors[ $color_scheme ]->icon_colors;
-		} elseif ( ! empty( $_wp_admin_css_colors['fresh']->icon_colors ) ) {
-			$icon_colors = $_wp_admin_css_colors['fresh']->icon_colors;
-		} else {
-			// Fall back to the default set of icon colors if the default scheme is missing.
-			$icon_colors = array(
-				'base'    => '#a0a5aa',
-				'focus'   => '#00a0d2',
-				'current' => '#fff',
-			);
-		}
-
-		return $icon_colors['base'];
 	}
 }
 
