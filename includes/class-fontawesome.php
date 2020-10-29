bce23a5d020bad3ae642d7836c5b6fe32eba9a9e
Mike Wilkerson <mwilkerson@gmail.com>
2020-10-29 15:24:18 -0700

    update conflict detection CDN asset version

--- includes/class-fontawesome.php
+++ includes/class-fontawesome.php
@@ -178,7 +178,7 @@ class FontAwesome {
 	 * @ignore
 	 * @internal
 	 */
-	const CONFLICT_DETECTOR_SOURCE = 'https://use.fontawesome.com/releases/v5.12.1/js/conflict-detection.js';
+	const CONFLICT_DETECTOR_SOURCE = 'https://use.fontawesome.com/releases/v5.15.1/js/conflict-detection.js';
 
 	/**
 	 * The custom data attribute added to script, link, and style elements enqueued
