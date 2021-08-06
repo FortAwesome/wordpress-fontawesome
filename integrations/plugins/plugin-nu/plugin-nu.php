e98cd3a011a7dfdcd1955a23afb199c6df16569b
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-08-06 15:12:07 -0700

    change plugin-nu to always add another tinymce editor

--- integrations/plugins/plugin-nu/plugin-nu.php
+++ integrations/plugins/plugin-nu/plugin-nu.php
@@ -12,27 +12,11 @@
 
 defined( 'WPINC' ) || die;
 
-function nu_is_gutenberg_page() {
-  if ( function_exists( 'is_gutenberg_page' ) && is_gutenberg_page() ) {
-    // The Gutenberg plugin is on.
-    return true;
-  }
-  $current_screen = get_current_screen();
-  if ( method_exists( $current_screen, 'is_block_editor' ) && $current_screen->is_block_editor() ) {
-    // Gutenberg page on 5+.
-    return true;
-  }
-
-  return false;
-}
-
 add_action('admin_print_footer_scripts', function() {
-  if( nu_is_gutenberg_page() ) {
-	?>
-	<div class="plugin-nu-content" style="border: 1px solid grey; margin-left: 200px; width: 50%;">
-	<h2>Plugin Nu</h2>
-	<?php wp_editor('(The block editor shoudl be working above, and the Icon Chooser should be useable in it.)', 'plugin-nu-editor');?>
-	</div>
-	<?php
-  }
+?>
+<div class="plugin-nu-content" style="border: 1px solid grey; margin-left: 200px; width: 50%;">
+<h2>Plugin Nu</h2>
+<?php wp_editor("(If there's a block editor above, it should be working, and the Icon Chooser should be useable in it, and this editor should not have an Add Font Awesome media button. If this is not a Gutenberg page, then this editor should have an Add Font Awesome media button that works.)", 'plugin-nu-editor');?>
+</div>
+<?php
 });
