6562ed3ed12637bd8bec708b80d53c7e98e9cb1d
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-08-17 13:35:40 -0700

    create a separate JS bundle for wp4 compat

---
+++ wp4-compat-js/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as i18n from '@wordpress/i18n'
+import * as element from '@wordpress/element'
+import * as components from '@wordpress/components'
+import apiFetch from '@wordpress/api-fetch'
+
+window.__Font_Awesome_Webpack_Externals__ = {
+  ...(window.__Font_Awesome_Webpack_Externals__ || {}),
+  React,
+  ReactDOM,
+  i18n,
+  element,
+  components,
+  apiFetch
+}
