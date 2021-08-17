a9c2026458a7260849f18c60e56e36ad89d9aa0e
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-08-17 14:18:34 -0700

    Update admin bundle package.json and webpack config

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -97,90 +97,19 @@ module.exports = {
      * from that global.
      */
     function (context, request, callback) {
-      // If any of the @wordpress modules depend upon one another, then it must
-      // be happening within our compat.js bundle. None of those should be
-      // externalized, because that's the one place where they actually belong.
-      if(/node_modules\/\@wordpress\//.test(request) || /node_modules\/\@wordpress\//.test(context) ) {
-        return callback()
-      }
-
       switch(request) {
         case 'react':
-          if(
-            /reactCompatWp4$/.test(context)
-            || /node_modules\/react-dom\//.test(context)
-            // When the packaage trying to include react is one of the
-            // @wordpress packages.
-            || /node_modules\/\@wordpress\//.test(context)
-            // The rest of these can be discovered by doing "npm ls react"
-            // and see which things depend on react. Ideally, we'd just have
-            // a rule that says "any time we depend on react from within the compat.js
-            // bundle, do not externalize," but that doesn't seem to be one
-            // of our options, at least in Webpack 4.
-            || /node_modules\/react-resize-aware/.test(context)
-            || /node_modules\/\@emotion\/core/.test(context)
-           ) {
-            callback()
-          } else {
-            return callback(null, 'root __Font_Awesome_Webpack_Externals__.React')
-          }
-          break;
+          return callback(null, 'root __Font_Awesome_Webpack_Externals__.React')
         case 'react-dom':
-          if(
-            /reactCompatWp4$/.test(context)
-            // For when @wordpress/element depends on react-dom
-            //|| /node_modules\/\@wordpress\/element\//.test(context)
-            || /node_modules\/\@wordpress\//.test(context)
-          ) {
-            callback()
-          } else {
-            return callback(null, 'root __Font_Awesome_Webpack_Externals__.ReactDOM')
-          }
-          break;
-          /*
+          return callback(null, 'root __Font_Awesome_Webpack_Externals__.ReactDOM')
         case '@wordpress/i18n':
-          if(
-            /reactCompatWp4$/.test(context)
-            // For when @wordpress/components depends on @wordpress/i18n
-            // || /node_modules\/\@wordpress\/api-fetch\//.test(context)
-            // || /node_modules\/\@wordpress\/components\//.test(context)
-
-            // For when any of the @wordpress modules depend on @
-            || /node_modules\/\@wordpress\//.test(context)
-          ) {
-            callback()
-          } else {
-            return callback(null, 'root __Font_Awesome_Webpack_Externals__.i18n')
-          }
-          break;
+          return callback(null, 'root __Font_Awesome_Webpack_Externals__.i18n')
         case '@wordpress/api-fetch':
-          if( /reactCompatWp4$/.test(context) ) {
-            callback()
-          } else {
-            return callback(null, 'root __Font_Awesome_Webpack_Externals__.apiFetch')
-          }
-          break;
+          return callback(null, 'root __Font_Awesome_Webpack_Externals__.apiFetch')
         case '@wordpress/components':
-          if( /reactCompatWp4$/.test(context) ) {
-            callback()
-          } else {
-            return callback(null, 'root __Font_Awesome_Webpack_Externals__.components')
-          }
-          break;
+          return callback(null, 'root __Font_Awesome_Webpack_Externals__.components')
         case '@wordpress/element':
-          if(
-            /reactCompatWp4$/.test(context)
-            // For when @wordpress/element depends on @wordpress/components
-            || /node_modules\/\@wordpress\/components\//.test(context)
-            // etc...
-            || /node_modules\/\@wordpress\/primitives\//.test(context)
-          ) {
-            callback()
-          } else {
-            return callback(null, 'root __Font_Awesome_Webpack_Externals__.element')
-          }
-          break;
-          */
+          return callback(null, 'root __Font_Awesome_Webpack_Externals__.element')
         default:
           return callback()
       }
