1281ded103fada031cc5ce06be8b924a43ccc489
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-08-18 13:48:46 -0700

    Add compatibility checks and warnings

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,5 +1,6 @@
 import { createStore } from './store'
 import get from 'lodash/get'
+import checkCompatibility from './checkCompatibility'
 
 let hasDomLoaded = false
 
@@ -53,8 +54,9 @@ if( !window.__Font_Awesome_Webpack_Externals__ ) {
   }
 }
 
+const { __ } = __Font_Awesome_Webpack_Externals__.i18n
+
 if(! initialData){
-  const { __ } = __Font_Awesome_Webpack_Externals__.i18n
   console.error( __( 'Font Awesome plugin is broken: initial state data missing.', 'font-awesome' ) )
 }
 
@@ -66,74 +68,88 @@ const {
   enableIconChooser
 } = store.getState()
 
+const compatible = checkCompatibility()
+
 if( showAdmin ) {
-  import('./mountAdminView')
-  .then(({ default: mountAdminView }) => {
-    mountAdminView(store, hasDomLoaded)
-  })
+  if( !compatible ) {
+    console.error( __( 'Font Awesome Plugin cannot show the admin page because it is not compatible with your WordPress installation.', 'font-awesome' ) )
+  } else {
+    import('./mountAdminView')
+    .then(({ default: mountAdminView }) => {
+      mountAdminView(store, hasDomLoaded)
+    })
+  }
 }
 
 if( showConflictDetectionReporter ) {
-  Promise.all([
-    import('./store/actions'),
-    import('./mountConflictDetectionReporter')
-  ])
-  .then(([{ reportDetectedConflicts }, { mountConflictDetectionReporter }]) => {
-    const report = params => store.dispatch(reportDetectedConflicts(params))
+  if( !compatible ) {
+    console.error( __( 'Font Awesome Plugin cannot load the conflict detection scanner because it is not compatible with your WordPress installation.', 'font-awesome' ) )
+  } else {
+    Promise.all([
+      import('./store/actions'),
+      import('./mountConflictDetectionReporter')
+    ])
+    .then(([{ reportDetectedConflicts }, { mountConflictDetectionReporter }]) => {
+      const report = params => store.dispatch(reportDetectedConflicts(params))
 
-    /**
-     * If the conflict detection report is already available, just use it;
-     * otherwise, listen for the reporting event.
-     */
-    if( conflictDetectionReport ) {
-      report(conflictDetectionReport)
-    } else {
-      document.addEventListener(
-        CONFLICT_DETECTION_REPORT_EVENT_TYPE,
-        _event => report(conflictDetectionReport)
-      )
-    }
+      /**
+       * If the conflict detection report is already available, just use it;
+       * otherwise, listen for the reporting event.
+       */
+      if( conflictDetectionReport ) {
+        report(conflictDetectionReport)
+      } else {
+        document.addEventListener(
+          CONFLICT_DETECTION_REPORT_EVENT_TYPE,
+          _event => report(conflictDetectionReport)
+        )
+      }
 
-    mountConflictDetectionReporter({
-      store,
-      now: hasDomLoaded
+      mountConflictDetectionReporter({
+        store,
+        now: hasDomLoaded
+      })
     })
-  })
+  }
 }
 
 if ( enableIconChooser ) {
-  Promise.all([
-    import('./chooser'),
-    import('./chooser/handleQuery'),
-    import('./chooser/getUrlText')
-  ])
-  .then(([{ setupIconChooser }, { default: configureQueryHandler }, { default: getUrlText } ]) => {
-    const kitToken = get(initialData, 'options.kitToken')
-    const version = get(initialData, 'options.version')
+  if( !compatible ) {
+    console.warn( __( 'Font Awesome Plugin cannot enable the Icon Chooser because it is not compatible with your WordPress installation.', 'font-awesome' ) )
+  } else {
+    Promise.all([
+      import('./chooser'),
+      import('./chooser/handleQuery'),
+      import('./chooser/getUrlText')
+    ])
+    .then(([{ setupIconChooser }, { default: configureQueryHandler }, { default: getUrlText } ]) => {
+      const kitToken = get(initialData, 'options.kitToken')
+      const version = get(initialData, 'options.version')
 
-    const params = {
-      ...initialData,
-      kitToken,
-      version,
-      getUrlText,
-      pro: get(initialData, 'options.usePro')
-    }
+      const params = {
+        ...initialData,
+        kitToken,
+        version,
+        getUrlText,
+        pro: get(initialData, 'options.usePro')
+      }
 
-    const handleQuery = configureQueryHandler(params)
+      const handleQuery = configureQueryHandler(params)
 
-    const { setupClassicEditorIconChooser } = setupIconChooser({ ...params, handleQuery })
+      const { setupClassicEditorIconChooser } = setupIconChooser({ ...params, handleQuery })
 
-    /**
-     * Tiny MCE will probably be loaded later, but since this code runs async,
-     * we can't guarantee the timing. So if this runs first, it will set this
-     * global to a function that the post-tiny-mce inline code can invoke.
-     * But if that code runs first, it will set this global to some truthy value,
-     * which tells us to invoke this setup immediately.
-     */
-    if( window['__FontAwesomeOfficialPlugin__setupClassicEditorIconChooser'] ) {
-      setupClassicEditorIconChooser()
-    } else {
-      window['__FontAwesomeOfficialPlugin__setupClassicEditorIconChooser'] = setupClassicEditorIconChooser
-    }
-  })
+      /**
+       * Tiny MCE will probably be loaded later, but since this code runs async,
+       * we can't guarantee the timing. So if this runs first, it will set this
+       * global to a function that the post-tiny-mce inline code can invoke.
+       * But if that code runs first, it will set this global to some truthy value,
+       * which tells us to invoke this setup immediately.
+       */
+      if( window['__FontAwesomeOfficialPlugin__setupClassicEditorIconChooser'] ) {
+        setupClassicEditorIconChooser()
+      } else {
+        window['__FontAwesomeOfficialPlugin__setupClassicEditorIconChooser'] = setupClassicEditorIconChooser
+      }
+    })
+  }
 }
