e4d66581770c363c45fc7c45ff6304019afb648c
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-08-30 12:52:57 -0700

    bump version for pre-release

--- includes/class-fontawesome.php
+++ includes/class-fontawesome.php
@@ -126,7 +126,7 @@ class FontAwesome {
 	 *
 	 * @since 4.0.0
 	 */
-	const PLUGIN_VERSION = '4.0.3-rc1';
+	const PLUGIN_VERSION = '4.0.3-rc2';
 	/**
 	 * The namespace for this plugin's REST API.
 	 *
