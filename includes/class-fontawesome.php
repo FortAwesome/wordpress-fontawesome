a22082f05a3908efc4299d2d947212f739431ee4
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-08-30 08:56:22 -0700

    Remove wp-editor and wp-blocks deps when integrating with Classic Editor (#146)
    * do not declare dependence on wp-blocks or wp-editor unless it's a Gutenberg page

--- includes/class-fontawesome.php
+++ includes/class-fontawesome.php
@@ -126,7 +126,7 @@ class FontAwesome {
 	 *
 	 * @since 4.0.0
 	 */
-	const PLUGIN_VERSION = '4.0.2';
+	const PLUGIN_VERSION = '4.0.3-rc1';
 	/**
 	 * The namespace for this plugin's REST API.
 	 *
@@ -1686,7 +1686,14 @@ class FontAwesome {
 		} else {
 			$deps = array_merge( $deps, array( 'react', 'react-dom', 'wp-i18n', 'wp-element', 'wp-components', 'wp-api-fetch' ) );
 
-			if ( $enable_icon_chooser ) {
+			/**
+			 * We don't need these Gutenberg dependencies unless we're on a Gutenberg
+			 * page. Declaring them unnecessarily (when not on a Gutenberg page)
+			 * has resulted in conflict for at least one other plugin: RankMath.
+			 *
+			 * See: https://wordpress.org/support/topic/plugin-conflicts-with-rankmath
+			 */
+			if ( $enable_icon_chooser && $this->is_gutenberg_page() ) {
 				$deps = array_merge( $deps, array( 'wp-blocks', 'wp-editor' ) );
 			}
 		}
