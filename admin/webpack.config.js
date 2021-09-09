6fd68e75acdaf29e9990d60bb4e098dcffae4489
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-09-09 09:53:11 -0700

    add hashes to webpack chunk output file names

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -2,8 +2,21 @@ const defaultConfig = require( '@wordpress/scripts/config/webpack.config' )
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const get = require('lodash/get')
 
+/**
+ * There have apparently been problems with webpack chunk loading and caches.
+ * So we should add hashes to the chunk file names.
+ * See: https://wordpress.org/support/topic/plugin-settings-page-is-empty-2/#post-14855904
+ */
+const miniCssExtractPlugin = defaultConfig.plugins.find(p => 'MiniCssExtractPlugin' === p.constructor.name)
+miniCssExtractPlugin.options.filename = '[name]-[contenthash].css'
+miniCssExtractPlugin.options.chunkFilename = '[name]-[chunkhash].css'
+
 module.exports = {
   ...defaultConfig,
+  output: {
+    ...defaultConfig.output,
+    chunkFilename: '[name]-[chunkhash].js'
+  },
   plugins: [
     ...defaultConfig.plugins,
     new BundleAnalyzerPlugin({ analyzerMode: 'static', reportFilename: '../webpack-stats.html', openAnalyzer: false })
