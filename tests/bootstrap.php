1b873a040be6cb46a0bdf3e55c2c3894d46a17d4
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2022-01-14 12:18:46 -0800

    update main test bootstrap for phpunit polyfill

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,39 +1,23 @@
 <?php
-/**
- * PHPUnit bootstrap file
- *
- * @package Font_Awesome
- * @noinspection PhpCSValidationInspection
+use Yoast\WPTestUtils\WPIntegration;
+
+if ( getenv( 'WP_PLUGIN_DIR' ) !== false ) {
+    define( 'WP_PLUGIN_DIR', getenv( 'WP_PLUGIN_DIR' ) );
+}
+
+$GLOBALS['wp_tests_options'] = [
+    'active_plugins' => [ 'font-awesome/index.php' ],
+];
+
+require_once dirname( __DIR__ ) . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
+
+/*
+ * Bootstrap WordPress. This will also load the Composer autoload file, the PHPUnit Polyfills
+ * and the custom autoloader for the TestCase and the mock object classes.
  */
+WPIntegration\bootstrap_it();
 
-define( 'FONTAWESOME_ENV', 'test' );
-
-// phpcs:ignoreFile Generic.Commenting.DocComment.MissingShort
-// phpcs:ignore WordPress.PHP.DevelopmentFunctions
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+if ( ! defined( 'WP_PLUGIN_DIR' ) || file_exists( WP_PLUGIN_DIR . '/font-awesome/index.php' ) === false ) {
+    echo PHP_EOL, 'ERROR: Please check whether the WP_PLUGIN_DIR environment variable is set and set to the correct value. The integration test suite won\'t be able to run without it.', PHP_EOL;
+    exit( 1 );
 }
-
-if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	error_log( "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL );
-	exit( 1 );
-}
-
-// Give access to tests_add_filter() function.
-/** @noinspection PhpIncludeInspection */
-require_once $_tests_dir . '/includes/functions.php';
-
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-	require_once dirname( dirname( __FILE__ ) ) . '/index.php';
-}
-set_include_path( get_include_path() . PATH_SEPARATOR . dirname( dirname( __FILE__ ) . '../' ) );
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-// Start up the WP testing environment.
-/** @noinspection PhpIncludeInspection */
-require_once $_tests_dir . '/includes/bootstrap.php';
