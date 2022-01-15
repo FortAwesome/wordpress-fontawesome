f9b303cd6a8dcd1bbe8f24f17d5e1e45c1a09572
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2022-01-14 19:15:41 -0800

    fix bootstrap and plugin dir for CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,7 +10,7 @@ if ( getenv( 'WP_PLUGIN_DIR' ) !== false ) {
 print "\n\nWP_PLUGIN_DIR: " . WP_PLUGIN_DIR . "\n\n";
 
 $GLOBALS['wp_tests_options'] = [
-    'active_plugins' => [ 'font-awesome/index.php' ],
+    'active_plugins' => [ 'index.php' ],
 ];
 
 require_once dirname( __DIR__ ) . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
@@ -21,7 +21,7 @@ require_once dirname( __DIR__ ) . '/vendor/yoast/wp-test-utils/src/WPIntegration
  */
 WPIntegration\bootstrap_it();
 
-if ( ! defined( 'WP_PLUGIN_DIR' ) || file_exists( WP_PLUGIN_DIR . '/font-awesome/index.php' ) === false ) {
+if ( ! defined( 'WP_PLUGIN_DIR' ) || file_exists( WP_PLUGIN_DIR . '/index.php' ) === false ) {
     echo PHP_EOL, 'ERROR: Please check whether the WP_PLUGIN_DIR environment variable is set and set to the correct value. The integration test suite won\'t be able to run without it.', PHP_EOL;
     exit( 1 );
 }
