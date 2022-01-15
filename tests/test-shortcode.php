f08debae9a18d6159be33a2b14a7f03f114f751d
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2022-01-14 17:54:40 -0800

    use assertMatchesRegularExpression

--- tests/test-shortcode.php
+++ tests/test-shortcode.php
@@ -14,46 +14,46 @@ class ShortcodeTest extends TestCase {
 	}
 
 	public function test_shortcode() {
-		$this->assertRegExp( '/<i class="fas fa-coffee">.*?<\/i>/', do_shortcode( '[icon name="coffee"/]' ) );
+		$this->assertMatchesRegularExpression( '/<i class="fas fa-coffee">.*?<\/i>/', do_shortcode( '[icon name="coffee"/]' ) );
 
-		$this->assertRegExp(
+		$this->assertMatchesRegularExpression(
 			'/<i class="far fa-bell">.*?<\/i>/',
 			do_shortcode( '[icon prefix="far" name="bell" /]' )
 		);
 
-		$this->assertRegExp(
+		$this->assertMatchesRegularExpression(
 			'/<i class="fas fa-coffee fa-2x foo">.*?<\/i>/',
 			do_shortcode( '[icon class="fa-2x foo" name="coffee" /]' )
 		);
 
-		$this->assertRegExp(
+		$this->assertMatchesRegularExpression(
 			'/<i class="fas fa-coffee" style="color: red;">.*?<\/i>/',
 			do_shortcode( '[icon style="color: red;" name="coffee" /]' )
 		);
 
 		$content = do_shortcode( '[icon role="img" aria-label="blah" aria-labelledby="foo" aria-hidden="true" title="coffee" name="coffee" /]' );
 
-		$this->assertRegExp(
+		$this->assertMatchesRegularExpression(
 			'/<i.*?\sclass="fas fa-coffee".*?>.*?<\/i>/',
 			$content
 		);
-		$this->assertRegExp(
+		$this->assertMatchesRegularExpression(
 			'/<i.*?\srole="img".*?>.*?<\/i>/',
 			$content
 		);
-		$this->assertRegExp(
+		$this->assertMatchesRegularExpression(
 			'/<i.*?\stitle="coffee".*?>.*?<\/i>/',
 			$content
 		);
-		$this->assertRegExp(
+		$this->assertMatchesRegularExpression(
 			'/<i.*?\saria-hidden="true".*?>.*?<\/i>/',
 			$content
 		);
-		$this->assertRegExp(
+		$this->assertMatchesRegularExpression(
 			'/<i.*?\saria-labelledby="foo".*?>.*?<\/i>/',
 			$content
 		);
-		$this->assertRegExp(
+		$this->assertMatchesRegularExpression(
 			'/<i.*?\saria-label="blah".*?>.*?<\/i>/',
 			$content
 		);
