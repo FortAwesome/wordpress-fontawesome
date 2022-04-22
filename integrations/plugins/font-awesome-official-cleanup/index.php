c64ff272a8863ab1fae13c32d1587c61460468d1
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2022-04-21 22:24:34 -0700

    consolidate

--- integrations/plugins/font-awesome-official-cleanup/index.php
+++ integrations/plugins/font-awesome-official-cleanup/index.php
@@ -21,10 +21,6 @@ function plugin_dir_url() { return \plugin_dir_url( __FILE__ ); }
 
 function plugin_dir_path() { return \plugin_dir_path( __FILE__ ); }
 
-function create_admin_page() {
-	include_once( plugin_dir_path() . 'admin-view.php' );
-}
-
 function settings_page_url() {
   return admin_url( "options-general.php?page=" . plugin_name() );
 }
@@ -191,6 +187,38 @@ add_filter(
 	'plugin_action_links_' . plugin_file(),
 	'FontAwesomeCleanup\filter_action_links'	
 );
+
+function create_admin_page() {
+	$font_awesome_cleanup_nonce = wp_create_nonce( 'font_awesome_cleanup_nonce' ); 
+	$status = isset( $_GET['status'] ) ? $_GET['status'] : null;
+
+	?>
+		<div class="<?= plugin_name() ?>-wrapper">
+			<?php if ( current_user_can( 'manage_options' ) ) { ?>
+				<?php if ( font_awesome_plugin_is_active() ) { ?>
+					<p>The Font Awesome Official plugin is still active, so we can't do a clean up right now.</p>
+					<p>Deactivate it and return here to do the clean up.</p>
+				<?php } else { ?>
+					<?php if ( 'done' === $status ) { ?>
+						All done.
+					<?php } else if ( 'nope' === $status ) { ?>
+						Security check failed. Maybe logout and login again before retrying?
+					<?php } else { ?>
+						Clean up all Font Awesome Official plugin data in the WordPress database.
+
+						<form method="post" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>">
+							<input type="hidden" name="action" value="font_awesome_cleanup">
+							<input type="hidden" name="font_awesome_cleanup_nonce" value="<?= $font_awesome_cleanup_nonce ?>" />
+							<?php submit_button('Clean Up!'); ?>
+						</form>
+					<?php } ?>
+				<?php } ?>
+			<?php } else { ?>
+				Sorry, you are not allowed to use this tool.
+			<?php } ?>
+		</div>
+	<?php
+}
 
 if( is_admin() ){
     initialize_admin();
