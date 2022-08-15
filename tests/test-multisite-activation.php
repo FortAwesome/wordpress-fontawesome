e35205f77c779c1bb66f08d6896130664f9a5c94
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2022-08-15 12:09:38 -0700

    fix multisite activation test expectations for latest_version_6

--- tests/test-multisite-activation.php
+++ tests/test-multisite-activation.php
@@ -87,7 +87,7 @@ class MultisiteActivationTest extends TestCase {
 			FontAwesome_Activator::initialize();
 			$site_count       = 0;
 			$test_obj         = $this;
-			$expected_options = array_merge( FontAwesome::DEFAULT_USER_OPTIONS, array( 'version' => fa()->latest_version() ) );
+			$expected_options = array_merge( FontAwesome::DEFAULT_USER_OPTIONS, array( 'version' => fa()->latest_version_6() ) );
 
 			for_each_blog(
 				function( $blog_id ) use ( $test_obj, $expected_options, &$site_count ) {
@@ -149,7 +149,7 @@ class MultisiteActivationTest extends TestCase {
 
 		$options_for_main_blog_id = fa()->options();
 
-		$expected_options = array_merge( FontAwesome::DEFAULT_USER_OPTIONS, array( 'version' => fa()->latest_version() ) );
+		$expected_options = array_merge( FontAwesome::DEFAULT_USER_OPTIONS, array( 'version' => fa()->latest_version_6() ) );
 
 		/**
 		 * We'll first, separately ensure that the options are initialized on the main site,
