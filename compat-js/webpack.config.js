54ca3776c766de2e6a45014dee7075b67d5c7f23
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2023-07-13 12:13:58 -0700

    fixup config for compat-js build

--- compat-js/webpack.config.js
+++ compat-js/webpack.config.js
@@ -1,12 +1,23 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' )
+const defaultConfig = require('@wordpress/scripts/config/webpack.config')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const get = require('lodash/get')
+const webpack = require('webpack')
+const rimraf = require('rimraf')
 
 module.exports = {
   ...defaultConfig,
   plugins: [
     ...defaultConfig.plugins,
-    new BundleAnalyzerPlugin({ analyzerMode: 'static', reportFilename: '../webpack-stats.html', openAnalyzer: false })
+    new BundleAnalyzerPlugin({ analyzerMode: 'static', reportFilename: '../webpack-stats.html', openAnalyzer: false }),
+    new webpack.ProvidePlugin({ process: 'process/browser' }),
+    new (class {
+      apply(compiler) {
+        compiler.hooks.done.tap('Remove LICENSE', () => {
+          console.log('Remove LICENSE.txt')
+          rimraf.sync('./build/*.LICENSE.txt', { glob: true })
+        });
+      }
+    })()
   ],
   output: {
     filename: 'compat.js',
