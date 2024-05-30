114f9d6a6f1f2895919a7a4d8974c3e91277fead
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-05-29 17:09:24 -0700

    finish the new playwright test for block editing with icon chooser

---
+++ admin/src/playwright/blockEditorIconChooser.playwright.js
@@ -0,0 +1,40 @@
+import { Editor, test, expect, login, RequestUtils } from '@wordpress/e2e-test-utils-playwright'
+
+test.describe( 'blockEditorIconChooser', async () => {
+	test.beforeEach( async ( { admin } ) => {
+		await admin.createNewPost()
+	} )
+
+  test.use( {
+	  editor: async ( { page }, use ) => {
+		  await use( new Editor( { page } ) )
+	  },
+  } )
+
+  test('works', async ({ editor, page, pageUtils }) => {
+		await editor.insertBlock( {
+			name: 'core/paragraph',
+		} );
+		await page.keyboard.type( 'Here comes an icon: ' )
+
+		await editor.clickBlockToolbarButton( 'More' )
+
+		await pageUtils.pressKeys('Enter', 1)
+
+		await page.waitForSelector( 'fa-icon-chooser input#search' );
+
+    const searchResponsePromise = page.waitForResponse(
+      '**/font-awesome/v1/api*'
+    );
+
+    await page.locator( 'fa-icon-chooser input#search' ).fill('coffee')
+
+    await searchResponsePromise
+
+    await page.locator( 'fa-icon-chooser button.icon' ).first().click()
+
+    const blocks = await editor.getBlocks()
+    expect(blocks).toHaveLength(1)
+    expect(blocks[0].attributes.content).toMatch(/\[icon.*?\]$/)
+  })
+} )
