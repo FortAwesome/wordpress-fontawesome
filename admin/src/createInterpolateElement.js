a2313f83dfa6a43dd28a4235a955d5d7d771da64
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-05-30 12:06:00 -0700

    bring back the compatibility handling for createInterpolateElement

--- admin/src/createInterpolateElement.js
+++ admin/src/createInterpolateElement.js
@@ -1,3 +1,5 @@
-import { createInterpolateElement } from '@wordpress/element'
+import { __experimentalCreateInterpolateElement, createInterpolateElement as stableCreateInterpolateElement } from '@wordpress/element'
+
+const createInterpolateElement = stableCreateInterpolateElement || __experimentalCreateInterpolateElement
 
 export default createInterpolateElement
