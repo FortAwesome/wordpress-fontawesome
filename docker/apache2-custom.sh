ab98675c3d3df8444d07ba5a9255963260c190c7
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-06-03 14:04:36 -0700

    make mod_security more configurable when starting a container

--- docker/apache2-custom.sh
+++ docker/apache2-custom.sh
@@ -7,11 +7,15 @@ chown -Rf www-data:www-data /var/www/html/
 DEFINES=""
 
 if [ "$FONTAWESOME_ENV" = "development" ]; then
-	DEFINES="${DEFINES} -DDEVELOPMENT"
+  DEFINES="${DEFINES} -DDEVELOPMENT"
 fi
 
-if [ "$ALLOW_PUT_REQUESTS" = "true" ]; then
-	DEFINES="${DEFINES} -DAllowPutRequests"
+if [ "$ENABLE_MOD_SECURITY" = "true" ]; then
+  DEFINES="${DEFINES} -DEnableModSecurity"
+fi
+
+if [ "$ALLOW_ALL_REQUESTS_FOR_FONT_AWESOME" = "true" ]; then
+  DEFINES="${DEFINES} -DAllowAllRequestsForFontAwesome"
 fi
 
 exec apache2-foreground ${DEFINES}
