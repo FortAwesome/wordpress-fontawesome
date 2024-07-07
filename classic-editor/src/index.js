eaaf17cee452edc3b59e185c4538515ef9465c73
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-07-06 19:20:55 -0700

    update classic editor integration for openEvent param

--- classic-editor/src/index.js
+++ classic-editor/src/index.js
@@ -4,8 +4,17 @@ import { buildShortCodeFromIconChooserResult } from './shortcode'
 import get from 'lodash/get'
 import { GLOBAL_KEY } from '../../admin/src/constants'
 
-const { IconChooserModal, modalOpenEvent } = get(window, [GLOBAL_KEY, 'iconChooser'], {});
+const { IconChooserModal } = get(window, [GLOBAL_KEY, 'iconChooser'], {});
 const iconChooserContainerId = 'font-awesome-icon-chooser-container'
+
+const modalOpenEvent = new Event("classicEditorFontAwesomeIconChooserOpen", {
+  "bubbles": true,
+  "cancelable": false,
+});
+
+const openIconChooser = () => {
+  document.dispatchEvent(modalOpenEvent)
+}
 
 export function handleSubmit(event) {
   const insert = get(window, 'wp.media.editor.insert')
@@ -21,6 +30,7 @@ function setupClassicEditor() {
     ReactDOM.render(
       <IconChooserModal
         onSubmit={ handleSubmit }
+        openEvent={ modalOpenEvent }
       />,
       container
     )
@@ -35,6 +45,12 @@ class FontAwesomeOfficialPlugin {
     // and load the kit into the editor's iframe so that they are rendered in visual
     // mode. This would work whether they are sVG/JS or webfont/css
     editor.on('init', () => {
+      const button = document.querySelector('button.font-awesome-icon-chooser-media-button')
+
+      if(button) {
+        button.addEventListener('click', openIconChooser)
+      }
+
       // This allows SVGs like the following:
       /*
        * <span class="fa-icon"><svg class="svg-inline--fa" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z">&#xFFFD;</path></svg></span>
