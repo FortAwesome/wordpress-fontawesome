3d993b73f6f0a57f65d96762122d567f26575121
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-07-08 16:21:12 -0700

    factor out generateRandomString

--- block-editor/src/createCustomEvent.js
+++ block-editor/src/createCustomEvent.js
@@ -1,16 +1,8 @@
+import generateRandomString from './generateRandomString'
+
 export default function (name) {
   return new Event(name || `fontAwesomeIconChooser-${generateRandomString(16)}`, {
     "bubbles": true,
     "cancelable": false,
   });
-}
-
-function generateRandomString(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
 }
