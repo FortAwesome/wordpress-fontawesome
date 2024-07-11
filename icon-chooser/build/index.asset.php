23e49c06b68f3afd1a1d8f3b6bfa91fa74f6c01c
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-07-11 12:57:21 -0700

    re-build production icon chooser bundle

--- icon-chooser/build/index.asset.php
+++ icon-chooser/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'fecb4e0ebdd7cdd6cb6b');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'e58c525ab89e26f4ca02');
