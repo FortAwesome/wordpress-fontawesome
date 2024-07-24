6b9813da3e386970d174ded2b20b75f39ab71420
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-07-23 21:02:40 -0700

    migrate admin bundle to use createRoot()

--- admin/src/mountAdminView.js
+++ admin/src/mountAdminView.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import ErrorBoundary from './ErrorBoundary'
 import FontAwesomeAdminView from './FontAwesomeAdminView'
 import { Provider } from 'react-redux'
 import domReady from '@wordpress/dom-ready'
 
+const root = createRoot(document.getElementById('font-awesome-admin'))
+
 export default function(store) {
   domReady(() =>
-    ReactDOM.render(
+    root.render(
       <ErrorBoundary>
         <Provider store={ store }>
           <FontAwesomeAdminView/>
         </Provider>
-      </ErrorBoundary>,
-      document.getElementById('font-awesome-admin')
+      </ErrorBoundary>
     )
   )
 }
