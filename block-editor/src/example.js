62cb96ab26e150fae724f2bbc5e964e165719fb6
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-07-26 11:04:55 -0700

    wire-up an example in JS

---
+++ block-editor/src/example.js
@@ -0,0 +1,11 @@
+import { faGear } from '@fortawesome/free-solid-svg-icons';
+
+const example = {
+  attributes: {
+    iconLayers: [
+      {iconDefinition: faGear}
+    ]
+  }
+}
+
+export default example
--- block-editor/src/index.js
+++ block-editor/src/index.js
@@ -1,13 +1,15 @@
-import { registerBlockType } from '@wordpress/blocks'
-import save from './save'
-import metadata from './block.json'
-import { faBrandIcon } from './icons'
-import './richTextIcon'
-import './index.css'
-import { Edit } from './edit'
+import { registerBlockType } from '@wordpress/blocks';
+import { createEditComponent, Edit } from './edit';
+import save from './save';
+import metadata from './block.json';
+import { faBrandIcon } from './icons';
+import './richTextIcon';
+import './index.css';
+import example from './example';
 
 registerBlockType(metadata.name, {
   icon: faBrandIcon,
   edit: Edit,
-  save
-})
+  save,
+  example
+});
