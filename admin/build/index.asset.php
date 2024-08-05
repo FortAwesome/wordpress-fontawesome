20063c1d0778ed0b184c538a0644ccd4657271cd
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-08-05 16:28:03 -0700

    update admin bundle version and build

--- admin/build/index.asset.php
+++ admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'dceaa19662fa76b5e570');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'd636e60e36e629ce7fec');
