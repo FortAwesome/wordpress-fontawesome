2a0856fab727eee4a102aa88618d96d1f1247d0b
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-08-07 16:11:25 -0700

    update doc

--- includes/class-fontawesome-api-settings.php
+++ includes/class-fontawesome-api-settings.php
@@ -307,7 +307,7 @@ class FontAwesome_API_Settings {
 
 	/**
 	 * Returns a current access_token, if available. Attempts to refresh an
-	 * access_token if the one we have is near or past expiration and an api_token
+	 * access_token if the one we have is near or past expiration and if an api_token
 	 * is present.
 	 *
 	 * Returns WP_Error indicating any error when trying to refresh an access_token.
