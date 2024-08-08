b7f0cf5f2d97c9744cbe81467ef04ad310453187
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-08-08 09:11:27 -0700

    improve classic editor TinyMCE plugin, toward 1:1 relationship between icon chooser and TinyMCE instance

--- classic-editor/src/index.js
+++ classic-editor/src/index.js
@@ -3,30 +3,33 @@ import { createRoot } from 'react-dom/client'
 import { buildShortCodeFromIconChooserResult } from './shortcode'
 import get from 'lodash/get'
 import { GLOBAL_KEY } from '../../admin/src/constants'
+import createCustomEvent from '../../block-editor/src/createCustomEvent'
+import { __ } from '@wordpress/i18n'
 
 const { IconChooserModal } = get(window, [GLOBAL_KEY, 'iconChooser'], {})
-const iconChooserContainerId = 'font-awesome-icon-chooser-container'
 
-const modalOpenEvent = new Event('classicEditorFontAwesomeIconChooserOpen', {
-  bubbles: true,
-  cancelable: false
-})
+// Creates a new icon chooser with its own open event and submit handler.
+function newIconChooser(editor) {
+  if ('function' !== typeof editor?.insertContent) {
+    console.error(__('Font Awesome Plugin: failed to initialize TinyMCE plugin.', 'font-awesome'))
+    return () => {}
+  }
 
-const openIconChooser = () => {
-  document.dispatchEvent(modalOpenEvent)
-}
+  const modalOpenEvent = createCustomEvent()
 
-export function handleSubmit(event) {
-  const insert = get(window, 'wp.media.editor.insert')
-  insert && insert(buildShortCodeFromIconChooserResult(event.detail))
-}
+  const openIconChooser = () => {
+    document.dispatchEvent(modalOpenEvent)
+  }
 
-function setupClassicEditor() {
-  // TODO: make sure this setup happens only once per page, no matter how many
-  // editors there are.
-  const container = document.querySelector(`#${iconChooserContainerId}`)
+  function handleSubmit(event) {
+    editor.insertContent(buildShortCodeFromIconChooserResult(event.detail))
+  }
 
-  const root = createRoot(container)
+  const container = editor.container
+  const iconChooserWrapper = document.createElement('div')
+  container.appendChild(iconChooserWrapper)
+
+  const root = createRoot(iconChooserWrapper)
 
   if (container) {
     root.render(
@@ -36,38 +39,21 @@ function setupClassicEditor() {
       />
     )
   }
+
+  return openIconChooser
 }
 
 class FontAwesomeOfficialPlugin {
   constructor(editor, _url, _c) {
-    // Probably what I should do here:
-    // just let the [icon] shortcodes continue to be what's inserted here, for now.
-    // and turn them into <i> tags or spans for visual mode.
-    // and load the kit into the editor's iframe so that they are rendered in visual
-    // mode. This would work whether they are sVG/JS or webfont/css
     editor.on('init', () => {
       const button = document.querySelector('button.font-awesome-icon-chooser-media-button')
+
+      const openIconChooser = newIconChooser(editor)
 
       if (button) {
         button.addEventListener('click', openIconChooser)
       }
-
-      // This allows SVGs like the following:
-      /*
-       * <span class="fa-icon"><svg class="svg-inline--fa" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z">&#xFFFD;</path></svg></span>
-       *
-       * Notice:
-       * - the object replacement char 0xfffd is in there, as in the block editor's inline formats.
-       * - the viewbox is lowercase (addValidElements doesn't respect it when it's viewBox)
-       *
-       * TODO: Need to figure out how to navigate the caret past the icon so it can be deleted.
-       * Or to make it selectable by clicking.
-       */
-      // editor.schema.addValidElements('svg[class|viewbox|xmlns]')
-      // editor.schema.addValidElements('path[class|d]')
     })
-
-    setupClassicEditor()
   }
 }
 
