947494ef095e93d9a5b2c5c47515a90435b0d37d
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-08-08 16:14:13 -0700

    auto-formatting

--- block-editor/src/index.js
+++ block-editor/src/index.js
@@ -1,15 +1,15 @@
-import { registerBlockType } from '@wordpress/blocks';
-import { Edit } from './edit';
-import save from './save';
-import metadata from './block.json';
-import { faBrandIcon } from './icons';
-import './richTextIcon';
-import './index.css';
-import example from './example';
+import { registerBlockType } from '@wordpress/blocks'
+import { Edit } from './edit'
+import save from './save'
+import metadata from './block.json'
+import { faBrandIcon } from './icons'
+import './richTextIcon'
+import './index.css'
+import example from './example'
 
 registerBlockType(metadata.name, {
   icon: faBrandIcon,
   edit: Edit,
   save,
   example
-});
+})
