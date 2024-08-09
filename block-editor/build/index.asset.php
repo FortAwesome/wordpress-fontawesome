968b26f9231666ae78db1608f7a5489f78993dd0
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2024-08-09 15:54:42 -0700

    update block-editor to use the WordPress built-in lodash

--- block-editor/build/index.asset.php
+++ block-editor/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'afe496a8127ad14a2bb0');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '1efe6b2034f2be97de84');
