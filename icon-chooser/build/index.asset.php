24250b87177313f942380037e1500b0159fad3ae
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-02-13 14:53:12 -0800

    rebuild icon-chooser bundle using local build

--- icon-chooser/build/index.asset.php
+++ icon-chooser/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '943d0a965b3c957326df');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '4feb6534a96c0e7f6dec');
