9ba9b4cac5f1f71f1e756858adf73a77c7298563
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-02-14 12:02:08 -0800

    bump version 5.0.0-alpha3 [skip ci]

--- includes/class-fontawesome.php
+++ includes/class-fontawesome.php
@@ -136,7 +136,7 @@ class FontAwesome {
 	 *
 	 * @since 4.0.0
 	 */
-	public const PLUGIN_VERSION = '5.0.0-alpha2';
+	public const PLUGIN_VERSION = '5.0.0-alpha3';
 	/**
 	 * The namespace for this plugin's REST API.
 	 *
