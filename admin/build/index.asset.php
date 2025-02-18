4f8a9cd491c8c5b586ebca817634680976c9b9e0
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-02-18 14:37:42 -0800

    rebuild admin bundle

--- admin/build/index.asset.php
+++ admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '3b3b083a3fa7793c28cc');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'a2b1b461f356f889739c');
