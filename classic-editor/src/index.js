78ad75d4b1d2316b1949c582d2621f74598885e1
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-02-20 16:54:22 -0800

    rework classic editor script

--- classic-editor/src/index.js
+++ classic-editor/src/index.js
@@ -9,7 +9,7 @@ import { __ } from '@wordpress/i18n'
 const { IconChooserModal } = get(window, [GLOBAL_KEY, 'iconChooser'], {})
 
 // Creates a new icon chooser with its own open event and submit handler.
-function newIconChooser(editorContainer, editorInsert) {
+function newIconChooser(editorId, container, editorInsert) {
   const modalOpenEvent = createCustomEvent()
 
   const openIconChooser = () => {
@@ -17,12 +17,12 @@ function newIconChooser(editorContainer, editorInsert) {
   }
 
   function handleSubmit(event) {
-    editorInsert(buildShortCodeFromIconChooserResult(event.detail))
+    editorInsert(editorId, buildShortCodeFromIconChooserResult(event.detail))
   }
 
   const iconChooserWrapper = document.createElement('div')
 
-  editorContainer.appendChild(iconChooserWrapper)
+  container.appendChild(iconChooserWrapper)
 
   const root = createRoot(iconChooserWrapper)
 
@@ -36,29 +36,67 @@ function newIconChooser(editorContainer, editorInsert) {
   return openIconChooser
 }
 
-function initializeIconChooserForEditor(editorId, editorContainer, editorInsert) {
-  const button = document.querySelector(`#fawp-tinymce-${editorId}`)
+function isVisible(textarea) {
+  const computedStyle = window.getComputedStyle(textarea)
+  if (computedStyle.display === 'none') {
+    return false
+  }
 
-  if (!button) {
-    console.error(__('Font Awesome Plugin: no Font Awesome media button found for TinyMCE editor id:', 'font-awesome'), editor.id)
+  if (computedStyle.visibility === 'hidden') {
+    return false
+  }
+
+  const rect = textarea.getBoundingClientRect()
+  if (rect.width === 0 || rect.height === 0) {
+    return false
+  }
+
+  return true
+}
+
+// Determine whether it's a TinyMCE editor or QTags editor, and
+// insert the content accordingly.
+function insertContentIntoEditor(editorId, content) {
+  const tinymceEditor = get(window, `tinymce.editors.${editorId}`)
+
+  if (tinymceEditor && !tinymceEditor.hidden) {
+    tinymceEditor.insertContent(content)
     return
   }
 
-  const openIconChooser = newIconChooser(editorContainer, editorInsert)
+  const qtagsEditor = window.QTags && QTags.getInstance(editorId)
+  const isQtagsEditorVisible = isVisible(qtagsEditor.canvas)
 
-  if (button) {
+  if (qtagsEditor && isQtagsEditorVisible) {
+    QTags.insertContent(content, editorId)
+  }
+}
+
+function initialize() {
+  const editorIds = get(window, '__FontAwesomeOfficialPlugin_tinymce__.editors', [])
+
+  for (const editorId of editorIds) {
+    const button = document.querySelector(`#fawp-tinymce-${editorId}`)
+    const editor = document.querySelector(`#${editorId}`)
+    const editorContainer = editor.parentElement
+
+    if (!editor || !button || !editorContainer) {
+      console.error(__('Font Awesome Plugin: could not attach to editor id:', 'font-awesome'), editorId)
+      continue
+    }
+
+    const openIconChooser = newIconChooser(editorId, editorContainer, (editorId, content) => {
+      insertContentIntoEditor(editorId, content)
+    })
+
     button.addEventListener('click', openIconChooser)
   }
 }
 
-class FontAwesomeOfficialPlugin {
-  constructor(editor, _url, _c) {
-    editor.on('init', () => {
-      initializeIconChooserForEditor(editor.id, editor.container, editor.insertContent.bind(editor))
-    })
-  }
-}
-
-if (window.tinymce) {
-  tinymce.PluginManager.add('font-awesome-official', FontAwesomeOfficialPlugin)
+if (document.readyState === 'complete') {
+  initialize()
+} else {
+  window.addEventListener('DOMContentLoaded', () => {
+    initialize()
+  })
 }
