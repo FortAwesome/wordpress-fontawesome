a2992fccb9f16bf0b7878ba4509035aace8dc4ba
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-02-27 15:21:34 -0800

    use the warning version of fetch_svg_styles from the config controller too

--- includes/class-fontawesome-config-controller.php
+++ includes/class-fontawesome-config-controller.php
@@ -135,7 +135,13 @@ class FontAwesome_Config_Controller extends WP_REST_Controller {
 				$db_item
 			);
 
-			FontAwesome_SVG_Styles_Manager::instance()->fetch_svg_styles( fa(), $this->release_provider() );
+			/**
+			 * The admin notices will not be shown as a consequence of any failure in this
+			 * function call, since this is a REST controller.
+			 * However, this will allow us attempt the fetch, without it causing a fatal error
+			 * if it fails.
+			 */
+			FontAwesome_SVG_Styles_Manager::instance()->fetch_svg_styles_with_admin_notice_warning( fa(), $this->release_provider() );
 
 			$return_data = $this->build_item( fa() );
 			return new FontAwesome_REST_Response( $return_data, 200 );
