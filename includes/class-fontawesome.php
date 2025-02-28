fd2a8d7f2956805587d0eb7d4cc31166ff06af1b
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-02-28 09:42:43 -0800

    pre-release 5.0.2-1

--- includes/class-fontawesome.php
+++ includes/class-fontawesome.php
@@ -136,7 +136,7 @@ class FontAwesome {
 	 *
 	 * @since 4.0.0
 	 */
-	public const PLUGIN_VERSION = '5.0.1';
+	public const PLUGIN_VERSION = '5.0.2-1';
 	/**
 	 * The namespace for this plugin's REST API.
 	 *
