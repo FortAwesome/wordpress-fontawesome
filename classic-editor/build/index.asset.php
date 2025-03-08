16be5d4c360f92c5fe751e3f926d90e78bfeb9d1
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-03-07 16:41:49 -0800

    rebuild classic-editor bundle

--- classic-editor/build/index.asset.php
+++ classic-editor/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-i18n'), 'version' => '16532cdc2e9643455c8b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-i18n'), 'version' => '7d5db1cbc6bf460346ff');
