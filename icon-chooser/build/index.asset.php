4853468c55f15839d043229b15a7c0365e4d0007
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-07-22 11:16:27 -0700

    re-build icon chooser bundle [skip ci]

--- icon-chooser/build/index.asset.php
+++ icon-chooser/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'd648f1a40985a3007e6f');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '661eb8679b822151fa27');
