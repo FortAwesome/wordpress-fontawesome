38c838e9e557891e6a8f7f31c45b697171674448
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-09-25 13:33:43 -0700

    Update some JS deps to clear dependabot alerts (#267)
    * update axios dep
    
    * rebuild admin js bundle
    
    * update playwright to update tar-fs dep for security
    
    * update tar-fs dep
    
    * update tar-fs

--- admin/build/index.asset.php
+++ admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'c533036ab18a17f3f9bc');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'ddeaddd96d83a4dbf5f3');
