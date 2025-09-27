c9ade3645d2e9972cdb63ccb33c0b9bf6d773c36
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-09-27 10:28:25 -0700

    fix up configs for using docker compose in CI

---
+++ bin/setup-wordpress-ci.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $DIR/../.env
+source $DIR/../.env.ci
+
+rc=`curl \
+  --write-out '%{http_code}' \
+  --silent \
+  --output /dev/null \
+  "http://$WP_DOMAIN/wp-admin/install.php?step=2" \
+  --data-urlencode "weblog_title=$WP_DOMAIN"\
+  --data-urlencode "user_name=$WORDPRESS_DB_USER" \
+  --data-urlencode "admin_email=$WP_ADMIN_EMAIL" \
+  --data-urlencode "admin_password=$WP_ADMIN_PASSWORD" \
+  --data-urlencode "admin_password2=$WP_ADMIN_PASSWORD" \
+  --data-urlencode "pw_weak=1"`
+
+if [ "$rc" == "200" ]; then
+  echo "SUCCESS initializing WordPress\n\tAdmin dashboard here:\n\thttp://$WP_DOMAIN/wp-admin"
+else
+  echo "FAIL with HTTP $rc"
+  exit 1
+fi
