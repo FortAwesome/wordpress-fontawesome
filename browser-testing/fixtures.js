266f6a1743725d813b7bb0ef0ef13ab6bd4a8f97
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-10-01 16:58:58 -0700

    fix fixtures.js

--- browser-testing/fixtures.js
+++ browser-testing/fixtures.js
@@ -1,6 +1,6 @@
 import * as playwright from '@wordpress/e2e-test-utils-playwright'
 
-export const test = base.extend({
+export const test = playwright.test.extend({
   page: async ({ page }, use) => {
     page.on('console', msg => {
       if (process.env.SHOW_CONSOLE === 'true') {
