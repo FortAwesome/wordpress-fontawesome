5594fb5976bac5553f3e3aaefc4978a84b109eec
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-10-01 14:01:16 -0700

    use dotenv-expand to DRY out env files

--- admin/src/playwright/support/env.js
+++ admin/src/playwright/support/env.js
@@ -1,12 +1,25 @@
-import dotenv from 'dotenv'
 import path from 'path'
+import dotenv from 'dotenv'
+import dotenvExpand from "dotenv-expand"
+import { accessSync } from 'fs'
 
 const ROOT_DIR = path.resolve(__dirname, '../../../..')
 
-dotenv.config({ path: path.resolve(ROOT_DIR, '.env'), override: true })
+const envFile = process.env.CI === 'true' ? '.env.ci' : '.env.local'
 
-if (process.env.CI === 'true') {
-  dotenv.config({ path: path.resolve(ROOT_DIR, '.env.ci'), override: true })
-} else {
-  dotenv.config({ path: path.resolve(ROOT_DIR, '.env.local'), override: true })
+dotenvExpand.expand(
+  dotenv.config({ path: path.resolve(ROOT_DIR, '.env'), override: true })
+)
+
+let foundEnvFile = false
+
+try {
+  accessSync()
+  foundEnvFile = true
+} catch {}
+
+if (foundEnvFile) {
+  dotenvExpand.expand(
+    dotenv.config({ path: path.resolve(ROOT_DIR, envFile), override: true })
+  )
 }
