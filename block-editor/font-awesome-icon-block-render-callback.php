f3918b7d827e4db812caceb17d52939c9c20a5be
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-10-03 11:44:15 -0700

    auto-formatting

--- block-editor/font-awesome-icon-block-render-callback.php
+++ block-editor/font-awesome-icon-block-render-callback.php
@@ -11,10 +11,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function font_awesome_icon_render_callback( $attributes ) {
-	$svg = '';
+	$svg         = '';
 	$icon_layers = $attributes['iconLayers'] ?? null;
 
-	if ( is_array( $icon_layers ) && !empty( $icon_layers ) ) {
+	if ( is_array( $icon_layers ) && ! empty( $icon_layers ) ) {
 		// We do not yet support multiple layers in this application, so just use the first one.
 		$svg = render_svg_from_icon_layer( $icon_layers[0] ) ?? '';
 	}
@@ -23,65 +23,72 @@ function font_awesome_icon_render_callback( $attributes ) {
 	return $html;
 }
 
-function render_svg_from_icon_layer( $icon_layer = [] ) {
-	if ( !is_array( $icon_layer ) || empty( $icon_layer ) ) {
+function render_svg_from_icon_layer( $icon_layer = array() ) {
+	if ( ! is_array( $icon_layer ) || empty( $icon_layer ) ) {
 		return;
 	}
 
 	$icon_definition = $icon_layer['iconDefinition'] ?? null;
 
-	if ( null === $icon_definition || !is_array( $icon_definition ) ) {
+	if ( null === $icon_definition || ! is_array( $icon_definition ) ) {
 		return;
 	}
 
 	$icon_name = $icon_definition['iconName'] ?? null;
 
-	if ( null === $icon_name || !is_string( $icon_name ) ) {
+	if ( null === $icon_name || ! is_string( $icon_name ) ) {
 		return;
 	}
 
 	$prefix = $icon_definition['prefix'] ?? null;
 
-	if ( null === $prefix || !is_string( $prefix ) ) {
+	if ( null === $prefix || ! is_string( $prefix ) ) {
 		return;
 	}
 
 	$icon_data = $icon_definition['icon'] ?? null;
 
-	if ( null === $icon_data || !is_array( $icon_data ) || count( $icon_data ) < 5 ) {
+	if ( null === $icon_data || ! is_array( $icon_data ) || count( $icon_data ) < 5 ) {
 		return;
 	}
 
 	$width = $icon_data[0];
 
-	if ( !is_numeric( $width ) ) {
+	if ( ! is_numeric( $width ) ) {
 		return;
 	}
 
 	$height = $icon_data[1];
 
-	if ( !is_numeric( $height ) ) {
+	if ( ! is_numeric( $height ) ) {
 		return;
 	}
 
-	$svg = '<svg aria-hidden="true" focusable="false" data-prefix="' . esc_attr( $prefix ) . '" data-icon="' . esc_attr( $icon_name ) . '" class="svg-inline--fa fa-' . esc_attr( $icon_name ) . '" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="' . esc_attr( "0 0 $width $height" ) . '">';
+	$svg = '<svg aria-hidden="true" focusable="false" data-prefix="'
+		. esc_attr( $prefix )
+		. '" data-icon="' . esc_attr( $icon_name )
+		. '" class="svg-inline--fa fa-'
+		. esc_attr( $icon_name )
+		. '" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="'
+		. esc_attr( "0 0 $width $height" )
+		. '">';
 
 	$path_or_paths = $icon_data[4];
 
-	$normalized_paths = [];
+	$normalized_paths = array();
 
 	if ( is_string( $path_or_paths ) ) {
-		$normalized_paths = [ $path_or_paths ];
+		$normalized_paths = array( $path_or_paths );
 	} elseif ( is_array( $path_or_paths ) ) {
 		$normalized_paths = $path_or_paths;
 	}
 
 	foreach ( $normalized_paths as $index => $path ) {
-		if ( !is_string( $path ) ) {
+		if ( ! is_string( $path ) ) {
 			continue;
 		}
 
-		$opacity_attribute = count( $normalized_paths ) > 1 && 0 === $index
+		$opacity_attribute       = count( $normalized_paths ) > 1 && 0 === $index
 			? 'opacity="0.4" '
 			: $opacity_attribute = '';
 
