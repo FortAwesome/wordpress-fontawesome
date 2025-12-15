1a3d48330caac843e6291faf5193b8a9b86d8c7a
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2025-12-15 10:52:13 -0800

    rebuild icon-chooser bundle

--- icon-chooser/build/index.asset.php
+++ icon-chooser/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '1ffd38231f1a41762e04');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'f3ae637211d837c4c702');
