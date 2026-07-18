b709eecc3b8712af8e019ae3e32176c9e68fc1b5
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2026-07-18 12:17:44 -0700

    update fa-icon-chooser-react and rebuild bundle

--- icon-chooser/build/index.asset.php
+++ icon-chooser/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '4e5660190a53caef49b4');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'af9cc0661d12be609d45');
