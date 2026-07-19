ae8e4617b5b86b367ddee0d9c838ee222a2b5349
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2026-07-19 12:30:10 -0700

    rebuild icon chooser JS bundle

--- icon-chooser/build/index.asset.php
+++ icon-chooser/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'af9cc0661d12be609d45');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '67aff315643884ae20cb');
