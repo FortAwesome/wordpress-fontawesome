d2e53896be770ced964ae4711226fcf202c925e5
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2026-07-27 14:47:46 -0700

    Deps updates for dependabot (#300)
    * update shell-quote @wordpress/env simple-git in block-editor
    
    * update shell-quote in admin, classic-editor, and icon-chooser bundles
    
    * update websocket-driver in block-editor, classic-editor, icon-chooser, and admin bundles
    
    * update basic-ftp in all bundles
    
    * update lodash and lodash-es deps
    
    * update axios
    
    * update flatted
    
    * update immutable
    
    * update node-forge
    
    * update form-data
    
    * update minimatch
    
    * update svgo
    
    * rebuild icon-chooser bundle
    
    * rebuild admin bundle

--- icon-chooser/build/index.asset.php
+++ icon-chooser/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '8083686a9b79d331fb63');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'c6f75f9eb94bbd83af76');
