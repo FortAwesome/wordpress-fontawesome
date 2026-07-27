a5dcf0d7b6c91398ce88fae9e7db3f03c2dd1302
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2026-07-27 13:21:49 -0700

    rebuild block-editor JS bundle

--- block-editor/build/index.asset.php
+++ block-editor/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'e494e307b992cf319bc9');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'f8721dbfa77520d07c75');
