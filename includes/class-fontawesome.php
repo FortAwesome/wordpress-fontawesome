136e0acf3492185d8bac4ce93436fce23dfedd56
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2026-07-28 14:54:54 -0700

    bump versions to 5.2.1

--- includes/class-fontawesome.php
+++ includes/class-fontawesome.php
@@ -136,7 +136,7 @@ class FontAwesome {
 	 *
 	 * @since 4.0.0
 	 */
-	public const PLUGIN_VERSION = '5.2.0';
+	public const PLUGIN_VERSION = '5.2.1';
 	/**
 	 * The namespace for this plugin's REST API.
 	 *
